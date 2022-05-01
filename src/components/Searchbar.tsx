552a2bd1eb80305d505b84874d9800a861e33837
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-01 22:30:27 +0100

    style searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Searchbar = () => {
   return (
-    <div>
-      <input type="text" placeholder="Search Google" />
-    </div>
+    <input
+      type="text"
+      placeholder="Search Google"
+      className="bg-[#C4C4C4] px-10 p-5 m-2 rounded-lg text-center placeholder:text-black font-bold text-2xl transition-all outline-none"
+    />
   );
 };
 
