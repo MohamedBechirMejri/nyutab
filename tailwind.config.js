5466275a7bcc618911fc4a0b6a4e78136acdf2d8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-01 18:49:41 +0100

    add border pulse animatiopn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        borderFade: "borderFade .7s alternate infinite",
+      },
+      keyframes: {
+        borderFade: {
+          "0%": {
+            borderColor: "currentColor",
+          },
+          "100%": {
+            borderColor: "transparent",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
