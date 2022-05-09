5ad815691f2d4d2d6940ff5d9c1f8d88b8d2e322
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-09 14:15:09 +0100

    add bottom nav

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -27,27 +27,61 @@ const Sidebar = () => {
       link: "news",
     },
   ];
+  const navItems2 = [
+    {
+      name: "Settings",
+      link: "settings",
+    },
+  ];
   return (
-    <div className="w-full h-full max-w-full py-3 dark:text-white">
-      <h1 className="w-full p-4 text-xl font-bold text-center bg-slate-800">
-        nyutab
-      </h1>
-      <nav className="flex flex-col justify-between">
-        <ul>
-          {navItems.map(item => (
-            <li
-              key={item.name}
-              className="flex items-center justify-center w-full h-full p-4 font-medium transition-all rounded-lg cursor-pointer hover:bg-slate-700"
+    <div className="flex flex-col justify-between w-full h-full max-w-full py-3 dark:text-white ">
+      <div>
+        {" "}
+        <h1 className="w-full p-4 text-xl font-bold text-center ">nyutab</h1>
+        <nav className="flex flex-col">
+          <ul>
+            {navItems.map(item => (
+              <li
+                key={item.name}
+                className="flex items-center justify-center w-full p-4 font-medium transition-all rounded-lg cursor-pointer hover:bg-slate-700"
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className="flex flex-col justify-center gap-4 py-2">
+        <nav>
+          <ul>
+            {navItems2.map(item => (
+              <li
+                key={item.name}
+                className="flex items-center justify-center w-full p-4 font-medium transition-all rounded-lg cursor-pointer hover:bg-slate-700"
+              >
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <footer className="text-xs text-center text-gray-500">
+          <p>
+            Created With <span className="text-red-500">&hearts;</span> by{" "}
+            <a
+              href="https://MohamedBechirMejri.dev"
+              className="text-zinc-300 hover:underline hover:text-zinc-100"
             >
-              {item.name}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          <li></li>
-          <li>Settings</li>
-        </ul>
-      </nav>
+              Mohamed Bechir Mejri
+            </a>
+          </p>
+          <a
+            href="https://github.com/MohamedBechirMejri/nyutab"
+            className="hover:underline hover:text-gray-400"
+          >
+            Version 0.0.1
+          </a>
+        </footer>
+      </div>
     </div>
   );
 };
