bca30f9c0f03b0143890103035d4382bda291190
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-09 10:29:16 +0100

    add dark styling

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -5,7 +5,7 @@ const Searchbar = () => {
     <input
       type="text"
       placeholder="Search Google"
-      className="bg-[#C4C4C4] px-10 p-5 m-2 rounded-lg text-center placeholder:text-black font-bold text-2xl transition-all outline-none"
+      className="bg-[#C4C4C4] dark:bg-slate-800 dark:text-white dark:placeholder:text-white px-10 p-5 m-2 rounded-lg text-center placeholder:text-black font-bold text-2xl transition-all outline-none w-full hover:bg-slate-700"
     />
   );
 };
