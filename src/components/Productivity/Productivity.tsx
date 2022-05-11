f08368a1c250f23c24e83684d8d1731c58208dcd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-11 20:03:39 +0100

    position prod tab

--- src/components/Productivity/Productivity.tsx
+++ src/components/Productivity/Productivity.tsx
@@ -1,21 +1,12 @@
 import React from "react";
+import PomodoroClock from "./PomodoroClock";
+import TodoList from "./TodoList";
 
 const Productivity = () => {
   return (
-    <div className="grid grid-rows-[.5fr_5fr_1fr] grid-cols-[5fr_1fr] h-full w-full justify-items-center items-center justify-center gap-2 ">
-      <div></div>
-      <div></div>
-      <div className="grid grid-rows-[.5fr_5fr] h-full w-full">
-        <div></div>
-        <div className="flex items-center justify-center w-full h-full text-2xl font-bold text-center text-white"></div>
-      </div>
-      <div className="grid grid-rows-[.5fr_5fr] h-full w-full">
-        <div></div>
-      </div>
-      <div className="grid items-center justify-center w-full h-full grid-cols-2 gap-2 justify-items-center">
-        <div></div>
-        <div></div>
-      </div>
+    <div className="grid grid-rows-[1fr_1fr] grid-cols-[5fr_2fr] h-full w-full justify-items-center items-center justify-center gap-2 ">
+      <TodoList />
+      <PomodoroClock />
     </div>
   );
 };
