d2f7e041995dff9281e7ab2f2a6923afe810d3a8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-11 20:36:46 +0100

    add placeholders

--- src/components/Productivity/Productivity.tsx
+++ src/components/Productivity/Productivity.tsx
@@ -7,9 +7,11 @@ const Productivity = () => {
     <div className="grid grid-rows-[1fr_1fr] grid-cols-[5fr_2fr] h-full w-full justify-items-center items-center justify-center gap-2 ">
       <div className="grid w-full h-full grid-cols-2 grid-rows-1">
         <TodoList />
+        <div></div>
       </div>
-
       <PomodoroClock />
+      <div></div>
+      <div></div>
     </div>
   );
 };
