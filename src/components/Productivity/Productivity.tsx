03f325afd2d4622efe687695010d033f4226ba4d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-11 20:19:08 +0100

    tweak grid

--- src/components/Productivity/Productivity.tsx
+++ src/components/Productivity/Productivity.tsx
@@ -5,7 +5,10 @@ import TodoList from "./TodoList";
 const Productivity = () => {
   return (
     <div className="grid grid-rows-[1fr_1fr] grid-cols-[5fr_2fr] h-full w-full justify-items-center items-center justify-center gap-2 ">
-      <TodoList />
+      <div className="grid w-full h-full grid-cols-2 grid-rows-1">
+        <TodoList />
+      </div>
+
       <PomodoroClock />
     </div>
   );
