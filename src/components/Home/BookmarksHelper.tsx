4f78fb03dd09e6f5d98ce2aeb3c3ab45b08e6c46
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-16 11:58:03 +0100

    add basic bookmarks structure

---
+++ src/components/Home/BookmarksHelper.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const BookmarksHelper = ({
+  bookmark,
+}: {
+  bookmark: {
+    children: React.ReactNode;
+    id: number;
+    title: string;
+    url: string;
+  };
+}) => {
+  console.log(bookmark);
+
+  return bookmark.children ? (
+    <div key={bookmark.id} className="flex flex-col gap-2">
+      <h1 className="text-lg font-bold text-center">{bookmark.title}</h1>
+
+      {
+        //@ts-ignore
+        bookmark.children.map(child => (
+          <BookmarksHelper bookmark={child} key={child.id} />
+        ))
+      }
+    </div>
+  ) : (
+    <a href={bookmark.url} key={bookmark.id} className="flex items-center gap-2 p-1 transition-all rounded-lg hover:bg-slate-800">
+      {/* <img src={"" + new URL(bookmark.url).hostname + "/favicon.ico"} alt="" /> */}
+      {/* <img
+        src={
+          "http://www.getfavicon.org/get.pl?url=" +
+          new URL(bookmark.url).hostname
+        }
+        alt=""
+        className="w-4 h-4"
+      /> */}
+      {bookmark.title}
+    </a>
+  );
+};
+
+export default BookmarksHelper;
