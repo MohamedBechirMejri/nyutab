deae68545cea001892d1f661fb3c4c33ca5b5659
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 12:47:33 +0100

    add test data

--- src/Components/Entertainment/Entertainment.tsx
+++ src/Components/Entertainment/Entertainment.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Awesome from "../../Components/Awesome/Awesome";
+import { AwesomeEntertainmentData } from "../../Components/Awesome/AwesomeData/Entertainment";
 
 const Entertainment = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Awesome header="Awesome Entertainment" data={AwesomeEntertainmentData} />
+    </div>
+  );
 };
 
 export default Entertainment;
