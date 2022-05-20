66d4f7278c1dda77e90bc99d70b7eedc90a39429
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 14:44:58 +0100

    Add snake game placeholder

--- src/Components/Entertainment/Entertainment.tsx
+++ src/Components/Entertainment/Entertainment.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Awesome from "../../Components/Awesome/Awesome";
 import { AwesomeEntertainmentData } from "../../Components/Awesome/AwesomeData/Entertainment";
+import Snake from "./Snake/Snake";
 
 const Entertainment = () => {
   return (
     <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center ">
+      <Snake />
       <Awesome
         header="Awesome Entertainment"
         data={AwesomeEntertainmentData}
