59652d84505144f6d7ec1bd18c4d1a55dce9a812
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 19:26:52 +0100

    add keydown handler

--- src/Components/Entertainment/Snake/Snake.tsx
+++ src/Components/Entertainment/Snake/Snake.tsx
@@ -12,6 +12,18 @@ const Snake = () => {
   const [gameOver, setGameOver] = React.useState(false);
   const [speed, setSpeed] = React.useState(200);
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "ArrowUp" && direction !== "down") {
+            setDirection("up");
+        } else if (e.key === "ArrowDown" && direction !== "up") {
+            setDirection("down");
+        } else if (e.key === "ArrowLeft" && direction !== "right") {
+            setDirection("left");
+        } else if (e.key === "ArrowRight" && direction !== "left") {
+            setDirection("right");
+        }
+    };
+
   return (
     <div className="[grid-area:1/1/4/4] w-full h-full p-2 transition-all flex flex-col items-center justify-center gap-4">
       <div className="grid grid-cols-[repeat(10,35px)] grid-rows-[repeat(10,35px)] w-max h-max border">
@@ -30,10 +42,8 @@ const Snake = () => {
           );
         })}
       </div>
-
       <h2>Score: {score}</h2>
-
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center gap-2">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => {
