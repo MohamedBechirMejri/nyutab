a0a0c2f4601689f57fbdb8eea926641628d4bf3b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 15:24:55 +0100

    add more states

--- src/Components/Entertainment/Snake/Snake.tsx
+++ src/Components/Entertainment/Snake/Snake.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import generateCoords from "../../../Utils/generateCoords";
 
 const Snake = () => {
-  const width = 10;
-  const height = 10;
-  const [board, setBoard] = React.useState(generateCoords(width, height));
+  const xAxis = 10;
+  const yAxis = 10;
+  const [board, setBoard] = React.useState(generateCoords(xAxis, yAxis));
+  const [snake, setSnake] = React.useState([] as number[]);
+  const [direction, setDirection] = React.useState("right");
+  const [food, setFood] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [gameOver, setGameOver] = React.useState(false);
+  const [speed, setSpeed] = React.useState(200);
+
   return <div></div>;
 };
 
