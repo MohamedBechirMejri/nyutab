20ab25eac5c99c35cf9ce90e43b2e7b94b71ffd9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 02:26:09 +0100

    add awesome links

--- src/components/Productivity/Awesome.tsx
+++ src/components/Productivity/Awesome.tsx
@@ -1,10 +1,52 @@
 import React from "react";
-const AwesomeProductivity = () => {
+const Awesome = ({
+  header,
+  data,
+}: {
+  header: string;
+  data: [
+    {
+      title: string;
+      links: [
+        {
+          title: string;
+          url: string;
+          description: string;
+        }
+      ];
+    }
+  ];
+}) => {
   return (
     <div className=" [grid-area:1/3/5/6] overflow-scroll h-full w-full p-2">
-      <h1 className="w-full p-3 font-bold text-center">Awesome Productivity</h1>
+      <h1 className="w-full p-3 font-bold text-center">{header}</h1>
+      <div className="flex flex-col gap-2">
+        {data.map((section, index) => (
+          <div key={index} className="flex items-center gap-2">
+            <div>
+              <h2>{section.title}</h2>
+              <ul>
+                {section.links.map((link, index) => (
+                  <li
+                    key={index}
+                    className="p-1 transition-all rounded-lg hover:bg-slate-800"
+                  >
+                    <a href={link.url}>
+                      - <span className="font-medium">{link.title} </span> -
+                      <span className="text-sm text-slate-400">
+                        {" "}
+                        {link.description}
+                      </span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default AwesomeProductivity;
+export default Awesome;
