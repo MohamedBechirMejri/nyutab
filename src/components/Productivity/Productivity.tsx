4044ee69c9af15ce83a28f0fdc908d2822781e5e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 01:43:53 +0100

    Add AwesomeProd component

--- src/components/Productivity/Productivity.tsx
+++ src/components/Productivity/Productivity.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AwesomeProductivity from "./Awesome";
 import PomodoroClock from "./PomodoroClock";
 import Tasks from "./Tasks";
 
@@ -7,6 +8,7 @@ const Productivity = () => {
     <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center ">
       <Tasks />
       <PomodoroClock />
+      <AwesomeProductivity />
     </div>
   );
 };
