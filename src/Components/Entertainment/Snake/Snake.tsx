d5cf544723ed84d4457870407a26e3cc8ea12cd7
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 16:14:36 +0100

    show snake on board

--- src/Components/Entertainment/Snake/Snake.tsx
+++ src/Components/Entertainment/Snake/Snake.tsx
@@ -5,7 +5,7 @@ const Snake = () => {
   const xAxis = 10;
   const yAxis = 10;
   const [board, setBoard] = React.useState(generateCoords(xAxis, yAxis));
-  const [snake, setSnake] = React.useState([] as number[]);
+  const [snake, setSnake] = React.useState([2, 3, 4] as number[]);
   const [direction, setDirection] = React.useState("right");
   const [food, setFood] = React.useState(0);
   const [score, setScore] = React.useState(0);
@@ -15,9 +15,19 @@ const Snake = () => {
   return (
     <div className="[grid-area:1/1/3/3] w-full h-full p-2 transition-all">
       <div className="grid grid-cols-10 grid-rows-10 w-full h-full border">
-        {board.map(coord => (
-          <div className="bg-slate-500 p-3 border"></div>
-        ))}
+        {board.map(coord => {
+          console.log(coord);
+          console.log(snake);
+          console.log(snake.includes(coord));
+          return (
+            <div
+              key={coord}
+              className={` p-3 border ${
+                snake.includes(coord) ? " bg-green-500 " : "bg-slate-500"
+              }`}
+            />
+          );
+        })}
       </div>
     </div>
   );
