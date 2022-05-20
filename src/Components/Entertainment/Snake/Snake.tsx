713fd3b9af513e1824f01a4ceda2f7158eb50783
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-20 15:21:04 +0100

    add board

--- src/Components/Entertainment/Snake/Snake.tsx
+++ src/Components/Entertainment/Snake/Snake.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import generateCoords from "../../../Utils/generateCoords";
 
 const Snake = () => {
+  const width = 10;
+  const height = 10;
+  const [board, setBoard] = React.useState(generateCoords(width, height));
   return <div></div>;
 };
 
