4263c184768a54f516ca694dba593cee581eb335
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 04:40:28 +0100

    remove reddit placeholder

--- src/Components/Dev/Dev.tsx
+++ src/Components/Dev/Dev.tsx
@@ -4,9 +4,7 @@ import Reddit from "./Reddit/Reddit";
 const Dev = () => {
   return (
     <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center  font-mono">
-      <div className="[grid-area:1/1/7/3] border w-full h-full">
-        <Reddit />
-      </div>
+      <Reddit />
       <div className="[grid-area:1/3/3/5] border w-full h-full">
         Awesome Dev
       </div>
