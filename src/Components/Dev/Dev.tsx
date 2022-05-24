6a38921a849004151fafb8c4544f9b78c3076c93
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 08:22:10 +0100

    add awesome dev

--- src/Components/Dev/Dev.tsx
+++ src/Components/Dev/Dev.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import HackerNews from "./HackerNews/HackerNews";
 import Reddit from "./Reddit/Reddit";
+import Awesome from "../Awesome/Awesome";
+import { AwesomeDevData } from "../Awesome/AwesomeData/Dev";
 
 const Dev = () => {
   return (
     <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center  font-mono">
       <Reddit />
-      <div className="[grid-area:1/3/3/5] border w-full h-full">
-        Awesome Dev
-      </div>
+
+      <Awesome
+        header="Awesome Dev"
+        data={AwesomeDevData}
+        className="[grid-area:1/3/3/5] w-full h-full"
+      />
+
       <div className="[grid-area:1/5/7/7] border w-full h-full">Github</div>
       <HackerNews />
     </div>
