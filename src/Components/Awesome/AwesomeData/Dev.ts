2326453d9d35d60eead5ecb22cb292f8f4e18ec8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 08:21:56 +0100

    add awesome dev test data

---
+++ src/Components/Awesome/AwesomeData/Dev.ts
@@ -0,0 +1,22 @@
+export const AwesomeDevData = [
+    {
+        title: 'placeholder 1',
+        links: [
+            {
+                title: 'placeholder 1',
+                url: 'https.//placeholder.com',
+                description: 'placeholder 1'
+            }
+        ]
+    },
+    {
+        title: 'placeholder 2',
+        links: [
+            {
+                title: 'placeholder 2',
+                url: 'https.//placeholder.com',
+                description: 'placeholder 2'
+            }
+        ]
+    }
+]
