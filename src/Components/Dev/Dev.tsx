d252e8304efee396b6b5d75dbf77d172d75920cf
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 01:24:16 +0100

    position component

--- src/Components/Dev/Dev.tsx
+++ src/Components/Dev/Dev.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Dev = () => {
-  return <div></div>;
+  return (
+    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center "></div>
+  );
 };
 
 export default Dev;
