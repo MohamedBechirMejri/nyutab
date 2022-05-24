a9acd63a2e4adbe45d48d54f4a72bfc227b3623c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 01:42:19 +0100

    Add sub components placeholders

--- src/Components/Dev/Dev.tsx
+++ src/Components/Dev/Dev.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Dev = () => {
   return (
-    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center "></div>
+    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center">
+      <div className="[grid-area:1/1/7/3] border w-full h-full">Reddit</div>
+      <div className="[grid-area:1/3/3/5] border w-full h-full">Awesome Dev</div>
+      <div className="[grid-area:1/5/7/7] border w-full h-full">Github</div>
+      <div className="[grid-area:3/3/7/5] border w-full h-full">Hacker News</div>
+    </div>
   );
 };
 
