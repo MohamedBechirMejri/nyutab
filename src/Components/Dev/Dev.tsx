edd8f95543ade4ce76bdbe92659fd9cde1d2b28d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 03:47:48 +0100

    monospace font

--- src/Components/Dev/Dev.tsx
+++ src/Components/Dev/Dev.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Reddit from "./Reddit/Reddit";
 
 const Dev = () => {
   return (
-    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center">
-      <div className="[grid-area:1/1/7/3] border w-full h-full">Reddit</div>
-      <div className="[grid-area:1/3/3/5] border w-full h-full">Awesome Dev</div>
+    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center  font-mono">
+      <div className="[grid-area:1/1/7/3] border w-full h-full">
+        <Reddit />
+      </div>
+      <div className="[grid-area:1/3/3/5] border w-full h-full">
+        Awesome Dev
+      </div>
       <div className="[grid-area:1/5/7/7] border w-full h-full">Github</div>
-      <div className="[grid-area:3/3/7/5] border w-full h-full">Hacker News</div>
+      <div className="[grid-area:3/3/7/5] border w-full h-full">
+        Hacker News
+      </div>
     </div>
   );
 };
