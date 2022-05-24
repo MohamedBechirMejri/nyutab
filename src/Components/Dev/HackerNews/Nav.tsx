031d71601f0de5c17b01cae489a5b9d6ff203273
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 07:38:39 +0100

    style nav

--- src/Components/Dev/HackerNews/Nav.tsx
+++ src/Components/Dev/HackerNews/Nav.tsx
@@ -8,45 +8,53 @@ const Nav = ({
   setCurrentTab: (tab: string) => void;
 }) => {
   return (
-    <nav>
-      <ul className="flex justify-center gap-2">
-        <li
-          className={`${
-            currentTab === "top" ? "text-blue-500" : "text-gray-500"
-          }`}
-        >
-          <button onClick={() => setCurrentTab("top")}>Top</button>
-        </li>
-        <li
-          className={`${
-            currentTab === "new" ? "text-blue-500" : "text-gray-500"
-          }`}
-        >
-          <button onClick={() => setCurrentTab("new")}>New</button>
-        </li>
-        <li
-          className={`${
-            currentTab === "show" ? "text-blue-500" : "text-gray-500"
-          }`}
-        >
-          <button onClick={() => setCurrentTab("show")}>Show</button>
-        </li>
-        <li
-          className={`${
-            currentTab === "ask" ? "text-blue-500" : "text-gray-500"
-          }`}
-        >
-          <button onClick={() => setCurrentTab("ask")}>Ask</button>
-        </li>
-        <li
-          className={`${
-            currentTab === "jobs" ? "text-blue-500" : "text-gray-500"
-          }`}
-        >
-          <button onClick={() => setCurrentTab("jobs")}>Jobs</button>
-        </li>
-      </ul>
-    </nav>
+    <ul className="grid items-center justify-between w-full grid-cols-5 grid-rows-1 noscroll">
+      <li
+        className={`${
+          currentTab === "top"
+            ? "text-blue-500"
+            : "text-gray-500 hover:text-gray-400"
+        } transition-all text-center font-bold  p-3 `}
+      >
+        <button onClick={() => setCurrentTab("top")}>Top</button>
+      </li>
+      <li
+        className={`${
+          currentTab === "new"
+            ? "text-blue-500"
+            : "text-gray-500 hover:text-gray-400"
+        } transition-all text-center font-bold  p-3 `}
+      >
+        <button onClick={() => setCurrentTab("new")}>New</button>
+      </li>
+      <li
+        className={`${
+          currentTab === "show"
+            ? "text-blue-500"
+            : "text-gray-500 hover:text-gray-400"
+        } transition-all text-center font-bold  p-3 `}
+      >
+        <button onClick={() => setCurrentTab("show")}>Show</button>
+      </li>
+      <li
+        className={`${
+          currentTab === "ask"
+            ? "text-blue-500"
+            : "text-gray-500 hover:text-gray-400"
+        } transition-all text-center font-bold  p-3 `}
+      >
+        <button onClick={() => setCurrentTab("ask")}>Ask</button>
+      </li>
+      <li
+        className={`${
+          currentTab === "jobs"
+            ? "text-blue-500"
+            : "text-gray-500 hover:text-gray-400"
+        } transition-all text-center font-bold  p-3 `}
+      >
+        <button onClick={() => setCurrentTab("jobs")}>Jobs</button>
+      </li>
+    </ul>
   );
 };
 
