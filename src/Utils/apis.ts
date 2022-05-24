b3133df89fc129b62fe4443f8eba5943bcc0e286
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 06:40:18 +0100

    add getstories

---
+++ src/Utils/apis.ts
@@ -0,0 +1,9 @@
+export const getStories = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
