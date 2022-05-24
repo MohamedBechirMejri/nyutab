c51ae75a4fb0fa4e99fb3a014a66e08ffa97d4c5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-24 07:43:52 +0100

    add title

--- src/Components/Dev/HackerNews/HackerNews.tsx
+++ src/Components/Dev/HackerNews/HackerNews.tsx
@@ -5,7 +5,8 @@ import Tab from "./Tab";
 const HackerNews = () => {
   const [currentTab, setCurrentTab] = React.useState("top");
   return (
-    <div className="[grid-area:3/3/7/5] border w-full h-full">
+      <div className="[grid-area:3/3/7/5] w-full h-full pt-2 pb-8 overflow-y-scroll noscroll">
+          <h1 className="w-full font-bold text-center">Hacker News</h1>
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <Tab currentTab={currentTab} />
     </div>
