7aa96ea0c71c8dba6ae3d4aaaa436e1b020343fa
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 03:29:29 +0100

    add standings component

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Nav from "./Nav";
+import Standings from "./Standings";
 
 const F1 = () => {
   const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
     <div>
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
+      <Standings currentTab={currentTab} />
     </div>
   );
 };
