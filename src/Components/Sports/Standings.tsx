d5417ebf550c8a6913d1c4825a053ed1c3196c95
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 06:15:05 +0100

    add constructors standings

--- src/Components/Sports/Standings.tsx
+++ src/Components/Sports/Standings.tsx
@@ -23,32 +23,7 @@ const Standings = ({ currentTab }: { currentTab: string }) => {
     console.log(standingsData);
   }, [currentTab]);
 
-  return (
-    // <div className="grid w-full h-full grid-cols-1 gap-2 overflow-y-scroll">
-    //   <div key={uniqid()} className="grid grid-cols-[2.5rem_9rem_3.5rem_2.5rem_8rem] w-full grid-rows-1">
-    //     <h1 className=""> Pos. </h1>
-    //     <h1 className="">Driver</h1>
-    //     <h1 className="">Points</h1>
-    //     <h1 className="">Wins</h1>
-    //     <h1 className="">Team</h1>
-    //   </div>
-    //   {standingsData.map((standing: any) => {
-    //     return (
-    //       <div
-    //         key={uniqid()}
-    //         className="grid grid-cols-[2.5rem_9rem_3.5rem_2.5rem_8rem] w-full  grid-rows-1"
-    //       >
-    //         <h1 className="">{standing.position}</h1>
-    //         <h1 className="">
-    //           {standing.Driver.givenName} {standing.Driver.familyName}
-    //         </h1>
-    //         <h1 className="">{standing.points}</h1>
-    //         <h1 className="">{standing.wins}</h1>
-    //         <h1 className="">{standing.Constructors[0].name}</h1>
-    //       </div>
-    //     );
-    //   })}
-    // </div>
+  return currentTab === "driverstandings" ? (
     <table className="w-full overflow-y-scroll">
       <thead>
         <tr>
@@ -65,15 +40,39 @@ const Standings = ({ currentTab }: { currentTab: string }) => {
             <tr key={uniqid()} className="">
               <td className="pl-2 text-center">{standing.position}</td>
               <td className="pl-2">
-                {standing.Driver.givenName} {standing.Driver.familyName}
+                {standing.Driver &&
+                  `${standing.Driver.givenName} ${standing.Driver.familyName}`}
               </td>
               <td className="pl-2 text-center">{standing.points}</td>
               <td className="pl-2 text-center">
-                {standing.Constructors[0].name}
+                {standing.Constructors && standing.Constructors[0].name}
               </td>
             </tr>
           );
         })}
+      </tbody>
+    </table>
+  ) : (
+    <table className="w-full overflow-y-scroll">
+      <thead>
+        <tr>
+          <th className="pl-2 text-center">Pos.</th>
+          <th className="pl-2">Team</th>
+          <th className="pl-2 text-center">Points</th>
+        </tr>
+      </thead>
+      <tbody className="">
+        {standingsData.map((standing: any) => {
+          return (
+            <tr key={uniqid()} className="">
+              <td className="pl-2 text-center">{standing.position}</td>
+              <td className="pl-2">
+                {standing.Constructor && standing.Constructor.name}{" "}
+              </td>
+              <td className="pl-2 text-center">{standing.points}</td>
+            </tr>
+          );
+        })}{" "}
       </tbody>
     </table>
   );
