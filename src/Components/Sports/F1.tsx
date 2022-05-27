c13da06083e6a428d017ea8bc4cb1c3275577f27
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 06:03:29 +0100

    position component

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -5,7 +5,7 @@ import Standings from "./Standings";
 const F1 = () => {
   const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
-    <div>
+    <div className=" [grid-area:1/1/6/4] w-full h-full">
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <Standings currentTab={currentTab} />
     </div>
