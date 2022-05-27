6020dbecfadf7dca3f93f7424fad27bad241b2f0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 10:44:16 +0100

    block text selection

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -6,7 +6,7 @@ import Standings from "./Standings";
 const F1 = () => {
   const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
-    <div className=" [grid-area:1/5/7/7] w-full h-full overflow-y-scroll noscroll ">
+    <div className=" [grid-area:1/5/7/7] w-full h-full overflow-y-scroll noscroll select-none">
       <NextRace />
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <Standings currentTab={currentTab} />
