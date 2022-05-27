cdbf7e2f0b0f6675a4e954217d103c65749ef73c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 03:11:56 +0100

    add f1

--- src/Components/Sports/Sports.tsx
+++ src/Components/Sports/Sports.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import F1 from "./F1";
 
 const Sports = () => {
   return (
-    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center "></div>
+    <div className="grid items-center justify-center w-full h-full grid-cols-[repeat(6,minmax(0,1fr))] grid-rows-[repeat(6,minmax(0,1fr))] gap-2 justify-items-center ">
+      <F1 />
+    </div>
   );
 };
 
