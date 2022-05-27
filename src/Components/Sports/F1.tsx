5a10939ec13422a24de46ed5571fada1669c5af9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 06:32:56 +0100

    hide scroll

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -5,7 +5,7 @@ import Standings from "./Standings";
 const F1 = () => {
   const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
-    <div className=" [grid-area:1/1/6/4] w-full h-full">
+    <div className=" [grid-area:1/1/6/3] w-full h-full overflow-y-scroll noscroll">
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <Standings currentTab={currentTab} />
     </div>
