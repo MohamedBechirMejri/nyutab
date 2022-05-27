4808d92240f68d58081d921f53ce58e970b587fd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 07:34:52 +0100

    tweak grid

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -6,7 +6,7 @@ import Standings from "./Standings";
 const F1 = () => {
   const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
-    <div className=" [grid-area:1/1/6/3] w-full h-full overflow-y-scroll noscroll">
+    <div className=" [grid-area:1/1/7/3] w-full h-full overflow-y-scroll noscroll">
       <NextRace />
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <Standings currentTab={currentTab} />
