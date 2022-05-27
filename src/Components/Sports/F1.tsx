58d0b2b53e2e5795ce1af1ef555ca0334573e7c1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-05-27 06:45:15 +0100

    add next race component

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Nav from "./Nav";
+import NextRace from "./NextRace";
 import Standings from "./Standings";
 
 const F1 = () => {
   const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
     <div className=" [grid-area:1/1/6/3] w-full h-full overflow-y-scroll noscroll">
+      <NextRace />
       <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <Standings currentTab={currentTab} />
     </div>
