97342bf7b92640d72bdf6199809571e19262c35f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-01 04:18:03 +0100

    get weather from api

--- src/Components/Home/Weather.tsx
+++ src/Components/Home/Weather.tsx
@@ -1,7 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Weather = () => {
+  const [coords, setCoords] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
   const [currentWeather, setCurrentWeather] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setCoords({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://api.weather api.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${coords.latitude},${coords.longitude}`
+      );
+      const data = await response.json();
+      setCurrentWeather(data.current.temp_c);
+    };
+    fetchData();
+  }, [coords.latitude, coords.longitude]);
 
   return (
     <div className="w-full p-3 text-2xl font-medium text-center bg-transparent rounded-lg dark:text-white ">
