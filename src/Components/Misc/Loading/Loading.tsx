0c709f14419aa700bb395ba8a7527d4009e15f7b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-02 10:52:03 +0100

    restyle loading componnet

--- src/Components/Misc/Loading/Loading.tsx
+++ src/Components/Misc/Loading/Loading.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./Style.css";
 
 const Loading = () => {
+  const styles =
+    "absolute p-8 rounded-full loading dark:ring-slate-100 top-1/2 left-1/2 ring-slate-500 ring -translate-y-1/2 -translate-x-1/2 opacity-0";
+
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="relative w-full h-full">
       {" "}
-      <div className="loading"></div>
+      <div className={styles + " [animation-delay:0s!important]"} />
+      <div className={styles + " [animation-delay:.5s!important] "} />
+      <div className={styles + " [animation-delay:.75s!important] "} />
     </div>
   );
 };
