692f2828cfc2b05c042cb4469753acba248e9b37
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-29 17:56:12 +0100

    add memes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Reddit from "./Components/Dev/Reddit/Reddit"
 import HackerNews from "./Components/Dev/HackerNews/HackerNews"
 import F1 from "./Components/Sports/F1"
 import Tasks from "./Components/Productivity/Tasks"
+import Memes from './Components/Entertainment/Memes';
 function App() {
   return (
     <div className="h-screen max-h-screen p-2 overflow-hidden App dark:bg-slate-900 dark:text-white">
@@ -25,13 +26,15 @@ function App() {
         <div className="w-full h-full col-span-4 row-span-4 bg-yellow-500x0">
           <Tasks />
         </div>
-        <div className="w-full h-full col-span-4 row-span-5 bg-gray-500"></div>
+        <div className="w-full h-full col-span-4 row-span-6 bg-gray-500x0">
+          <Memes />
+        </div>
         <div className="w-full h-full col-span-4 row-span-3 bg-purple-500"></div>
         <div className="w-full h-full col-span-4 row-span-6 bg-pink-500x0">
           <HackerNews />
         </div>
         <div className="w-full h-full col-span-4 row-span-4 bg-slate-500"></div>
-        <div className="w-full h-full col-span-4 row-span-3 bg-cyan-500"></div>
+        <div className="w-full h-full col-span-4 row-span-2 bg-cyan-500"></div>
       </div>
     </div>
   )
---
+++ src/Components/Entertainment/Memes.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const Memes = () => {
+  const [meme, setMeme] = useState(null) as any
+
+  const getMeme = () => {
+    axios
+      .get("https://meme-api.herokuapp.com/gimme")
+      .then(res => setMeme(res.data))
+  }
+
+  useEffect(() => {
+    getMeme()
+  }, [])
+
+  return (
+    <div className="flex justify-center w-full h-full overflow-scroll rounded-xl">
+      {meme ? (
+        <div>
+          <img src={meme.url} alt="" className="h-full" />
+        </div>
+      ) : (
+        "loading"
+      )}
+    </div>
+  )
+}
+
+export default Memes
