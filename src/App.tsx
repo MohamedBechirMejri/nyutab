3a2168c661cb5cc51caa25d90f84e5f69246e811
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-31 13:58:06 +0100

    tweak design

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
     <div className="h-screen max-h-screen p-2 overflow-hidden App dark:bg-slate-900 dark:text-white">
       <div className="grid items-center justify-center w-full h-full grid-cols-12 gap-2 grid-rows-[repeat(12,minmax(0,1fr))] justify-items-center ">
         {/* add x0 to keep the color hints in case i need them */}
-
         <div className="w-full h-full col-span-9 bg-blackx0">
           <FavoriteSites />
         </div>
@@ -36,33 +35,46 @@ function App() {
         <div className="w-full h-full col-span-4 row-span-3 bg-blue-500x0">
           <F1 />
         </div>
-        <div className="w-full h-full col-span-4 row-span-4 bg-yellow-500x0">
-          <Tasks />
+        <div className="w-full h-full col-span-4 row-span-4 bg-yellow-500">
+          {/* <Tasks /> */}
         </div>
         <div className="w-full h-full col-span-4 row-span-6 bg-gray-500x0">
           <Memes />
         </div>
-        <div className="w-full h-full col-span-4 row-span-3 p-2 bg-purple-500x0">
-          <a
-            href="https://picsum.photos"
-            className="w-full h-full bg-[url(https://picsum.photos/1000)] bg-center bg-cover bg-no-repeat rounded-lg relative flex flex-col items-center justify-center overflow-hidden text-white hover:scale-[1.03] active:scale-100 transition-all"
-          >
-            <div className="absolute w-full h-full bg-black opacity-50"></div>
-            <p className="absolute top-0 left-0 p-2 text-xs">Sponsored By:</p>
-            <h1 className="z-10 text-3xl font-bold">Picsum Photos</h1>
-            <p className="z-10 text-sm">Random Images API</p>
-          </a>
-        </div>
+        <div className="w-full h-full col-span-4 row-span-5 p-2 bg-purple-500"></div>
         <div className="w-full h-full col-span-4 row-span-6 bg-pink-500x0">
           <HackerNews />
         </div>
-        <div className="w-full h-full col-span-4 row-span-4 bg-slate-500"></div>
         <div className="relative w-full h-full col-span-4 row-span-2 bg-cyan-500x0">
           {isQuote ? (
             <Quotes className="absolute transition-all -translate-x-1/2 -translate-y-1/2 opacity-0 top-1/2 left-1/2 animate-fadeIn" />
           ) : (
             <FunFacts className="absolute transition-all -translate-x-1/2 -translate-y-1/2 opacity-0 top-1/2 left-1/2 animate-fadeIn" />
           )}
+        </div>{" "}
+        <div className="grid w-full h-full grid-cols-2 col-span-4 row-span-2 gap-2 p-2 text-xl font-bold text-white bg-slate-500x0">
+          {/* <button className="w-full h-full text-teal-500 transition-all border border-current rounded-lg active:scale-95 hover:bg-[#14b8a527]">
+            Tools
+            <p className="text-xs">soon</p>
+          </button>
+          <button className="w-full h-full transition-all border border-current rounded-lg text-rose-500 active:scale-95 hover:bg-[#f43f5e27]">
+            Games
+            <p className="text-xs">soon</p>
+          </button> */}
+          <a
+            href="https://github.com/MohamedBechirMejri/nyutab/issues"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="w-full h-full transition-all border border-current rounded-lg text-fuchsia-500 active:scale-95 hover:bg-[#d946ef27]">
+              <span className="text-lg">Help us Improve</span>
+              <p className="text-xs">v0.7.0 alpha</p>
+            </button>
+          </a>
+          <button className="w-full h-full transition-all border border-current rounded-lg text-violet-500 active:scale-95 hover:bg-[#895bf327]">
+            Settings
+            <p className="text-xs">soon</p>
+          </button>
         </div>
       </div>
     </div>
