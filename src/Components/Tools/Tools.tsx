19cab7cc465acdbfa98ff8202433a6c643c155bc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-01 23:32:40 +0100

    remove soon prop

--- src/Components/Tools/Tools.tsx
+++ src/Components/Tools/Tools.tsx
@@ -7,7 +7,6 @@ const Tools = ({ setOverlay }: { setOverlay: any }) => {
       <div className="grid w-full h-max grid-cols-2 gap-4 p-4 grid-rows-[repeat(25,minmax(0,1fr))]">
         <Button
           name="Breathing Exercice"
-          soon={true}
           className="text-teal-500 hover:bg-[#14b8a527]"
           handleClick={() => setOverlay("breathing exercise")}
         />
