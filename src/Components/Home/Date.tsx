72a667f3e0d86999c70fbd6b391fc91176c5c6bc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-03 22:11:37 +0100

    add date component

---
+++ src/Components/Home/Date.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getDate } from "../../lib/dateUtils";
+
+const Date = () => {
+  const [currentDate, setCurrentDate] = useState(getDate());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(getDate());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <h1 className="p-3 font-medium text-center bg-transparent rounded-lg dark:text-white text-2xl font-[GreatVibes]">
+      <p className="">{currentDate}</p>
+    </h1>
+  );
+};
+
+export default Date;
