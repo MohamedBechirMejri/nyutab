35464a519c18f3f3bc3e83139f9525cecc8d7c8c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-03 23:35:48 +0100

    add transition effect

--- src/Components/Overlay.tsx
+++ src/Components/Overlay.tsx
@@ -1,5 +1,6 @@
 import { IoClose } from "react-icons/io5";
 import BreathingExercise from "./Tools/BreathingExercise";
+import { motion } from "framer-motion";
 
 const Overlay = ({
   overlay,
@@ -9,17 +10,29 @@ const Overlay = ({
   setOverlay: any;
 }) => {
   return (
-    <div className="w-full h-full p-14">
+    <motion.div className="w-full h-full p-14 absolute z-50 origin-top">
       <button
         className="text-4xl absolute top-6 right-6 hover:rotate-180 transition-all duration-300 active:scale-90"
         onClick={() => setOverlay("")}
       >
         <IoClose />
       </button>
-      <div className="w-full h-full bg-slate-900 rounded-xl ">
+      <motion.div
+        className="w-full h-full bg-slate-900 rounded-xl "
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          ease: "anticipate",
+          duration: 0.3,
+        }}
+      >
         {overlay === "breathing exercise" && <BreathingExercise />}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
