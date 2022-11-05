3ae3dc89cb2bd0058fd853de025f957d16da7658
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-06 00:58:24 +0100

    add themes selection

--- src/Components/Setup/Theme.tsx
+++ src/Components/Setup/Theme.tsx
@@ -53,37 +53,46 @@ const Theme = ({
     <div className="flex items-center justify-center flex-col gap-16">
       <h1 className="text-xl font-semibold ">Select Theme</h1>
       <div className="flex gap-6">
-        {themes.map(t => (
-          <motion.div
-            initial={{
-              padding: "0",
-            }}
-            animate={{
-              padding: ".75rem",
-              scale: 1,
-            }}
-            whileHover={{
-              padding: ".5rem",
-            }}
-            whileTap={{
-              scale: 0.9,
-            }}
-            className="cursor-pointer w-24 h-24 rounded-full flex items-center justify-center select-none"
-            style={{
-              backgroundColor: t.primary,
-            }}
-          >
-            <div
-              className="w-full h-full rounded-full flex items-center justify-center text-xs shadow-xl"
-              style={{
-                backgroundColor: t.secondary,
-                color: t.text,
+        {themes.map(t => {
+          const isSelected = JSON.stringify(theme) === JSON.stringify(t);
+
+          return (
+            <motion.div
+              key={JSON.stringify(t)}
+              initial={{
+                padding: ".5rem",
               }}
+              whileHover={{
+                y: isSelected ? 0 : -10,
+              }}
+              whileTap={{
+                padding: ".75rem",
+              }}
+              className="cursor-pointer w-24 h-24 rounded-full flex items-center justify-center select-none relative"
+              style={{
+                backgroundColor: t.primary,
+              }}
+              onClick={() => setTheme(t)}
             >
-              {GetColorName(t.primary.replace("#", ""))}
-            </div>
-          </motion.div>
-        ))}
+              {isSelected && (
+                <motion.span
+                  initial={{ opacity: 0, y: 15, scale: 0 }}
+                  animate={{ opacity: 1, y: 0, scale: 1 }}
+                  className="absolute w-2 h-2 bg-current rounded-full -bottom-8"
+                />
+              )}
+              <div
+                className="w-full h-full rounded-full flex items-center justify-center text-xs shadow-xl"
+                style={{
+                  backgroundColor: t.secondary,
+                  color: t.text,
+                }}
+              >
+                {GetColorName(t.primary.replace("#", ""))}
+              </div>
+            </motion.div>
+          );
+        })}
       </div>
     </div>
   );
