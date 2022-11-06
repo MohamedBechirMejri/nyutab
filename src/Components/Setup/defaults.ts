d9d7a2fbb99530cf8f91411a591f85e97d93f532
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-06 03:44:18 +0100

    add icon field

--- src/Components/Setup/defaults.ts
+++ src/Components/Setup/defaults.ts
@@ -2,61 +2,73 @@ export const FAVORITES = [
   {
     id: 1,
     name: "Google",
+    icon: "",
     url: "https://www.google.com",
   },
   {
     id: 2,
     name: "Facebook",
+    icon: "",
     url: "https://www.facebook.com",
   },
   {
     id: 3,
     name: "Youtube",
+    icon: "",
     url: "https://www.youtube.com",
   },
   {
     id: 4,
     name: "Amazon",
+    icon: "",
     url: "https://www.amazon.com",
   },
   {
     id: 5,
     name: "Netflix",
+    icon: "",
     url: "https://www.netflix.com",
   },
   {
     id: 6,
     name: "Spotify",
+    icon: "",
     url: "https://www.spotify.com",
   },
   {
     id: 7,
     name: "Twitch",
+    icon: "",
     url: "https://www.twitch.com",
   },
   {
     id: 8,
     name: "Github",
+    icon: "",
     url: "https://www.github.com",
   },
   {
     id: 9,
     name: "Reddit",
+    icon: "",
     url: "https://www.reddit.com",
   },
   {
     id: 10,
     name: "Twitter",
+    icon: "",
     url: "https://www.twitter.com",
   },
   {
     id: 11,
     name: "Instagram",
+    icon: "",
     url: "https://www.instagram.com",
   },
   {
     id: 12,
     name: "Twitch",
+    icon: "",
     url: "https://www.twitch.com",
   },
 ];
