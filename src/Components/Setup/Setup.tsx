584f682bf09c16315bcba86c11aaaadb95e8c558
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-06 01:37:41 +0100

    add buttons

--- src/Components/Setup/Setup.tsx
+++ src/Components/Setup/Setup.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { IoCheckmark } from "react-icons/io5";
 import Theme from "./Theme";
 
 const Setup = () => {
@@ -72,6 +74,18 @@ const Setup = () => {
     text: "#f5f5f5",
   });
 
+  const buttonAnimation = {
+    initial: {
+      scale: 1,
+    },
+    whileHover: {
+      x: [0, -5, 0, -5, 0],
+    },
+    whileTap: {
+      scale: 0.65,
+    },
+  };
+
   return (
     <motion.div
       initial={{
@@ -83,9 +97,26 @@ const Setup = () => {
       transition={{
         delay: 0.5,
       }}
-      className="w-full flex justify-center items-center h-full flex-col gap-8 pt-8 bg-slate-600 rounded-xl shadow-2xl"
+      className="w-full flex justify-center items-center h-full flex-col gap-32 pt-8 bg-slate-600 rounded-xl shadow-2xl relative"
     >
+      {/* <h1 className="absolute top-8 left-8 text-xl">
+        Hello, Let's setup your Homepage..
+      </h1> */}
       <Theme theme={theme} setTheme={SetTheme} />
+      <div className="flex items-center gap-24 text-4xl">
+        <motion.button {...buttonAnimation}>
+          <IoIosArrowBack />
+        </motion.button>
+        <motion.button
+          {...buttonAnimation}
+          whileHover={{
+            x: [0, 5, 0, 5, 0],
+          }}
+        >
+          <IoIosArrowForward />
+          {/* <IoCheckmark /> */}
+        </motion.button>
+      </div>
     </motion.div>
   );
 };
