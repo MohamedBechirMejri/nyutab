101d733e281753980cae4dba311d0d32493a99ee
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-08 04:08:08 +0100

    add nsfw toggle

--- src/Components/Overlay/Settings/Memes.tsx
+++ src/Components/Overlay/Settings/Memes.tsx
@@ -3,6 +3,13 @@ import { FiCheck, FiX } from "react-icons/fi";
 const Memes = ({ memes, setMemes }: { memes: any; setMemes: any }) => {
   const { sources } = memes;
 
+  const toggleNSFW = () => {
+    setMemes((memesSettings: any) => ({
+      ...memesSettings,
+      isNsfwEnabled: !memesSettings.isNsfwEnabled,
+    }));
+  };
+
   const toggleSource = (id: number) => {
     setMemes((memes: any) => {
       const { sources } = memes;
@@ -18,29 +25,49 @@ const Memes = ({ memes, setMemes }: { memes: any; setMemes: any }) => {
   };
 
   return (
-    <div className="">
-      <div className="flex flex-col gap-4">
-        {sources.map((meme: any, i: number) => (
-          <div
-            onClick={() => toggleSource(i)}
-            className={`rounded text-xl p-2 px-8 text-center transition-all cursor-pointer flex w-full justify-between items-center gap-12 ${
-              meme.isEnabled
-                ? "bg-green-500 text-green-500"
-                : "bg-red-500 text-red-500"
-            } select-none active:scale-95 bg-opacity-10 hover:bg-opacity-25`}
+    <div className="flex flex-col gap-4">
+      <div
+        onClick={() => toggleNSFW()}
+        className={`rounded text-xl p-2 px-8 text-center transition-all cursor-pointer flex w-full justify-between items-center gap-16  select-none `}
+      >
+        NSFW
+        <div
+          className={` transition-all w-12 rounded-full relative h-4  ${
+            memes.isNsfwEnabled ? "bg-green-500" : "bg-red-500"
+          } bg-opacity-25 `}
+        >
+          <span
+            className={`rounded-full transition-all ${
+              memes.isNsfwEnabled
+                ? "bg-green-500 text-green-800 left-[80%]"
+                : "bg-red-500 text-red-800 left-2"
+            } absolute top-1/2 -translate-y-1/2 -translate-x-1/2 text-base p-1`}
           >
-            r/{meme.name} {meme.isEnabled ? <FiCheck /> : <FiX />}
-          </div>
-        ))}
-        {/* // TODO: Will need to test this first and make sure users don't break it using links or wrong subreddits */}
-        {/* <input
+            {memes.isNsfwEnabled ? <FiCheck /> : <FiX />}
+          </span>
+        </div>
+      </div>
+      {sources.map((meme: any, i: number) => (
+        <div
+          key={i + meme.name}
+          onClick={() => toggleSource(i)}
+          className={`rounded text-xl p-2 px-8 text-center transition-all cursor-pointer flex w-full justify-between items-center gap-12 ${
+            meme.isEnabled
+              ? "bg-green-500 text-green-500"
+              : "bg-red-500 text-red-500"
+          } select-none active:scale-95 bg-opacity-10 hover:bg-opacity-25`}
+        >
+          r/{meme.name} {meme.isEnabled ? <FiCheck /> : <FiX />}
+        </div>
+      ))}
+      {/* // TODO: Will need to test this first and make sure users don't break it using links or wrong subreddits */}
+      {/* <input
           type="text"
           placeholder="Custom Subreddit"
           className="rounded bg-blue-500 bg-opacity-10 hover:bg-opacity-25 text-blue-500 outline-none border-none placeholder:text-blue-500 text-center"
         />
 
         */}
-      </div>
     </div>
   );
 };
