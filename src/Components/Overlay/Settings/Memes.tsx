7784e81845d4cdf83c9db0f1e19bb5206982e502
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-08 03:39:08 +0100

    add todo for future option

--- src/Components/Overlay/Settings/Memes.tsx
+++ src/Components/Overlay/Settings/Memes.tsx
@@ -32,6 +32,14 @@ const Memes = ({ memes, setMemes }: { memes: any; setMemes: any }) => {
             r/{meme.name} {meme.isEnabled ? <FiCheck /> : <FiX />}
           </div>
         ))}
+        {/* // TODO: Will need to test this first and make sure users don't break it using links or wrong subreddits */}
+        {/* <input
+          type="text"
+          placeholder="Custom Subreddit"
+          className="rounded bg-blue-500 bg-opacity-10 hover:bg-opacity-25 text-blue-500 outline-none border-none placeholder:text-blue-500 text-center"
+        />
+
+        */}
       </div>
     </div>
   );
