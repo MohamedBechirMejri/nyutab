57333e401caf716a66237815ad51cb79556431d5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-08 09:53:52 +0100

    get prayer times

--- src/Components/Overlay/Tools/NextPrayerButton.tsx
+++ src/Components/Overlay/Tools/NextPrayerButton.tsx
@@ -1,6 +1,30 @@
 import Button from "../../Misc/Button";
+import { useContext, useEffect, useState } from "react";
+import { SettingsContext } from "../../../lib/contexts";
+import { getPrayerTimes, savePrayerTimes } from "../../../lib/storageUtils";
+import getApiPrayerTimes from "../../../lib/getApiPrayerTimes";
 
 const NextPrayerButton = ({ setOverlay }: { setOverlay: any }) => {
+  const settings = useContext(SettingsContext)!;
+
+  const [prayerTimes, setPrayerTimes] = useState<any>(null);
+
+  useEffect(() => {
+    const savedPrayerTimes = getPrayerTimes();
+    const date = new Date();
+
+    if (!settings) return;
+
+    if (savedPrayerTimes && date === new Date(savedPrayerTimes.date)) {
+      setPrayerTimes(savedPrayerTimes);
+    } else {
+      getApiPrayerTimes(settings.city).then(newPrayerTimes => {
+        setPrayerTimes(newPrayerTimes);
+        savePrayerTimes(newPrayerTimes);
+      });
+    }
+  }, [settings]);
+
   return (
     <Button
       name="Fajr in 00:40:00"
