cba549b7d1b4aed628621154e8523f2fb5289639
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-08 12:28:34 +0100

    format code

--- src/Components/Sports/F1.tsx
+++ src/Components/Sports/F1.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react"
+// import React, { useState } from "react";
 // import Nav from "./Nav";
-import NextRace from "./NextRace"
+import NextRace from "./NextRace";
 // import Standings from "./Standings";
 
 const F1 = () => {
-  const [currentTab, setCurrentTab] = useState("driverstandings")
+  // const [currentTab, setCurrentTab] = useState("driverstandings");
   return (
     <div className="flex items-center w-full h-full overflow-y-scroll select-none  noscroll">
       <NextRace />
       {/* <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} /> */}
       {/* <Standings currentTab={currentTab} /> */}
     </div>
-  )
-}
+  );
+};
 
-export default F1
+export default F1;
