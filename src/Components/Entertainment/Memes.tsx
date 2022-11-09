9fe0a3b5e25d59627eaf12c2d72c8c9498f3650a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-09 07:10:43 +0100

    add action buttons

--- src/Components/Entertainment/Memes.tsx
+++ src/Components/Entertainment/Memes.tsx
@@ -1,32 +1,70 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { FiHeart, FiRefreshCw } from "react-icons/fi";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { RiHistoryLine } from "react-icons/ri";
+import { SettingsContext } from "../../lib/contexts";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Memes = () => {
-  const [meme, setMeme] = useState(null) as any
+  const settings = useContext(SettingsContext);
+
+  const [meme, setMeme] = useState(null) as any;
+  const [history, setHistory] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const [isHovered, setIsHovered] = useState(false);
 
   // TODO: add history, nsfw filter, more subs (from localstorage)
 
   const getMeme = () => {
     axios
       .get("https://meme-api.herokuapp.com/gimme")
-      .then(res => setMeme(res.data))
-  }
+      .then(res => setMeme(res.data));
+  };
 
   useEffect(() => {
-    getMeme()
-  }, [])
-
+    getMeme();
+  }, []);
+  console.log("res: ", meme);
   return (
     <div className="flex justify-center w-full h-full overflow-scroll rounded-xl">
       {meme ? (
-        <div>
+        <div
+          className="relative"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           <img src={meme.url} alt="" className="h-full rounded" />
+          <AnimatePresence>
+            {isHovered && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="absolute left-0 top-0 flex items-end justify-center gap-10 bg-[#00000088] w-full h-full text-4xl py-10"
+              >
+                <button>
+                  <FiHeart className="fill-red-500 stroke-red-400" />
+                </button>
+                <a href={meme.postLink} target="_blank" rel="noreferrer">
+                  <HiOutlineExternalLink />
+                </a>
+                <button>
+                  <RiHistoryLine />
+                </button>
+                <button>
+                  <FiRefreshCw />
+                </button>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       ) : (
         "loading"
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Memes
+export default Memes;
