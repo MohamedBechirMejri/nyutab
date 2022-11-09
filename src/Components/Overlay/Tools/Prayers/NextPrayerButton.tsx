656a34e537af19dbcbe8fb93b692412b423a74d4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-09 02:41:10 +0100

    comment out everything

--- src/Components/Overlay/Tools/Prayers/NextPrayerButton.tsx
+++ src/Components/Overlay/Tools/Prayers/NextPrayerButton.tsx
@@ -1,68 +1,60 @@
-import Button from "../../../Misc/Button";
-import { useContext, useEffect, useState } from "react";
-import { SettingsContext } from "../../../../lib/contexts";
-import { getPrayerTimes, savePrayerTimes } from "../../../../lib/storageUtils";
-import getApiPrayerTimes from "../../../../lib/getApiPrayerTimes";
-import getNextPrayer from "../../../../lib/getNextPrayer";
-import RCountdown from "react-countdown";
-import getTomorrowDate from "../../../../lib/getTomorrowDate";
-
 const NextPrayerButton = ({ setOverlay }: { setOverlay: any }) => {
-  const settings = useContext(SettingsContext)!;
+  // const settings = useContext(SettingsContext)!;
 
-  const [prayerTimes, setPrayerTimes] = useState<any>(null);
-  const [nextPrayer, setNextPrayer] = useState<any>(null);
+  // const [prayerTimes, setPrayerTimes] = useState<any>(null);
+  // const [nextPrayer, setNextPrayer] = useState<any>(null);
 
-  useEffect(() => {
-    const savedPrayerTimes = getPrayerTimes();
-    const date = new Date().toString().slice(0, 15);
+  // useEffect(() => {
+  //   const savedPrayerTimes = getPrayerTimes();
+  //   const date = new Date().toString().slice(0, 15);
 
-    if (!settings) return;
+  //   if (!settings) return;
 
-    if (
-      savedPrayerTimes &&
-      date === new Date(savedPrayerTimes.data.date).toString().slice(0, 15)
-    ) {
-      setPrayerTimes(savedPrayerTimes);
-    } else {
-      getApiPrayerTimes(settings.city).then(newPrayerTimes => {
-        setPrayerTimes(newPrayerTimes);
-        savePrayerTimes(newPrayerTimes);
-      });
-    }
-  }, [settings]);
+  //   if (
+  //     savedPrayerTimes &&
+  //     date === new Date(savedPrayerTimes.data.date).toString().slice(0, 15)
+  //   ) {
+  //     setPrayerTimes(savedPrayerTimes);
+  //   } else {
+  //     getApiPrayerTimes(settings.city).then(newPrayerTimes => {
+  //       setPrayerTimes(newPrayerTimes);
+  //       savePrayerTimes(newPrayerTimes);
+  //     });
+  //   }
+  // }, [settings]);
 
-  useEffect(() => {
-    if (!prayerTimes) return;
-    setNextPrayer(getNextPrayer(prayerTimes)[0]);
-  }, [prayerTimes]);
+  // useEffect(() => {
+  //   if (!prayerTimes) return;
+  //   setNextPrayer(getNextPrayer(prayerTimes)[0]);
+  // }, [prayerTimes]);
 
   return (
-    <RCountdown
-      date={
-        //  time > next prayer ? tomorrows date : today's date
-        new Date(
-          `${
-            new Date().toString().slice(16, 21) > nextPrayer?.time
-              ? getTomorrowDate().toString().slice(0, 15)
-              : new Date().toString().slice(0, 15)
-          } ${nextPrayer?.time}`
-        )
-      }
-      onComplete={() => {
-        setNextPrayer(getNextPrayer(prayerTimes)[0]);
-      }}
-      renderer={props => {
-        const { hours, minutes, seconds } = props.formatted;
-        return (
-          <Button
-            name={`${nextPrayer?.name} in ${`${hours}:${minutes}:${seconds}`}`}
-            className="text-teal-400 hover:bg-[#14b8a527]"
-            handleClick={() => setOverlay("prayers")}
-          />
-        );
-      }}
-    />
+    <div></div>
+    // <RCountdown
+    //   date={
+    //     //  time > next prayer ? tomorrows date : today's date
+    //     new Date(
+    //       // `${
+    //         // new Date().toString().slice(16, 21) > nextPrayer?.time
+    //           // ? getTomorrowDate().toString().slice(0, 15)
+    //           // : new Date().toString().slice(0, 15)
+    //       // } ${nextPrayer?.time}`
+    //     )
+    //   }
+    //   onComplete={() => {
+    //     setNextPrayer(getNextPrayer(prayerTimes)[0]);
+    //   }}
+    //   renderer={props => {
+    //     const { hours, minutes, seconds } = props.formatted;
+    //     return (
+    //       <Button
+    //         name={`${nextPrayer?.name} in ${`${hours}:${minutes}:${seconds}`}`}
+    //         className="text-teal-400 hover:bg-[#14b8a527]"
+    //         handleClick={() => setOverlay("prayers")}
+    //       />
+    //     );
+    //   }}
+    // />
   );
 };
 
