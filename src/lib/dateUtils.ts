cb4e1b8a6fd05bbbce9c5b70e5fede0f5d9d1827
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-09 01:51:15 +0100

    add date utils

--- src/lib/dateUtils.ts
+++ src/lib/dateUtils.ts
@@ -9,3 +9,15 @@ export const getDate = () => {
 export const getTime = () => {
   return new Date().toLocaleTimeString();
 };
+
+export const getTodaysDate = (date?: any) => {
+  return date ? new Date(date) : new Date();
+};
+export const getTomorrowsDate = () => {
+  const today = new Date();
+  // to return the date number(1-31) for the specified date
+  let tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  //returns the tomorrow date
+  return tomorrow;
+};
--- src/lib/getTomorrowDate.ts
+++
@@ -1,8 +0,0 @@
-const getTomorrowDate = () => {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-
-  return tomorrow;
-};
-export default getTomorrowDate;
