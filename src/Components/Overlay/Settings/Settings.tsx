1abca6c3314729432cff12ffd43a93b31f693ba0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-17 16:22:54 +0100

    format code

--- src/Components/Overlay/Settings/Settings.tsx
+++ src/Components/Overlay/Settings/Settings.tsx
@@ -44,11 +44,11 @@ const Settings = ({
       initial={{ scaleY: 0 }}
       animate={{ scaleY: 1 }}
       transition={{ delay: 0.5 }}
-      className="w-full grid grid-cols-6 grid-rows-1 h-full bg-slate-600 rounded-xl shadow-2xl relative"
+      className="relative grid w-full h-full grid-cols-6 grid-rows-1 shadow-2xl bg-slate-600 rounded-xl"
     >
-      <div className="col-span-1 flex flex-col justify-between items-center py-8">
+      <div className="flex flex-col items-center justify-between col-span-1 py-8">
         <div className="flex flex-col items-center gap-8">
-          {sections.map(s => (
+          {sections.map((s) => (
             <button
               key={s + "-" + uniqid()}
               className={`uppercase border w-full p-4 px-6 rounded-lg border-slate-700 ${
@@ -69,7 +69,7 @@ const Settings = ({
           </button>
         </div>
       </div>
-      <div className="col-span-5 flex items-center p-4 justify-center w-full overflow-scroll relative">
+      <div className="relative flex items-center justify-center w-full col-span-5 p-4 overflow-scroll">
         {section === "theme" && <Theme theme={theme} setTheme={SetTheme} />}
         {section === "favorites" && (
           <Favorites favorites={favorites} setFavorites={setFavorites} />
