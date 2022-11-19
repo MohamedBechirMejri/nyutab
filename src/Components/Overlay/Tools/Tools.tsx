b2170c0313d3c09a9110e28a0ddfa4c8d8ef2385
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-19 17:36:55 +0100

    fix typo

--- src/Components/Overlay/Tools/Tools.tsx
+++ src/Components/Overlay/Tools/Tools.tsx
@@ -7,7 +7,7 @@ const Tools = ({ setOverlay }: { setOverlay: any }) => {
       <h1 className="text-xl font-semibold">Tools</h1>
       <div className="grid w-full h-max grid-cols-2 gap-4 p-4 grid-rows-[repeat(25,minmax(0,1fr))]">
         <Button
-          name="Breathing Exercice"
+          name="Breathing Exercise"
           className="text-teal-500 hover:bg-[#14b8a527]"
           handleClick={() => setOverlay("breathing exercise")}
         />
