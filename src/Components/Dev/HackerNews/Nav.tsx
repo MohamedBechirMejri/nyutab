9a7660176b620f5371e7672ba22cf7d3d9173eda
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-19 17:21:27 +0100

    refactor component

--- src/Components/Dev/HackerNews/Nav.tsx
+++ src/Components/Dev/HackerNews/Nav.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SettingsContext } from "../../../lib/contexts";
 
 const Nav = ({
   currentTab,
@@ -7,53 +8,31 @@ const Nav = ({
   currentTab: string;
   setCurrentTab: (tab: string) => void;
 }) => {
+  const settings = useContext(SettingsContext);
+  const tabs = ["top", "new", "show", "ask", "jobs"];
+
   return (
     <ul className="grid items-center justify-between w-full grid-cols-5 grid-rows-1 noscroll">
-      <li
-        className={`${
-          currentTab === "top"
-            ? "text-blue-500"
-            : "text-gray-500 hover:text-gray-400"
-        } transition-all text-center font-bold  p-3 `}
-      >
-        <button onClick={() => setCurrentTab("top")}>Top</button>
-      </li>
-      <li
-        className={`${
-          currentTab === "new"
-            ? "text-blue-500"
-            : "text-gray-500 hover:text-gray-400"
-        } transition-all text-center font-bold  p-3 `}
-      >
-        <button onClick={() => setCurrentTab("new")}>New</button>
-      </li>
-      <li
-        className={`${
-          currentTab === "show"
-            ? "text-blue-500"
-            : "text-gray-500 hover:text-gray-400"
-        } transition-all text-center font-bold  p-3 `}
-      >
-        <button onClick={() => setCurrentTab("show")}>Show</button>
-      </li>
-      <li
-        className={`${
-          currentTab === "ask"
-            ? "text-blue-500"
-            : "text-gray-500 hover:text-gray-400"
-        } transition-all text-center font-bold  p-3 `}
-      >
-        <button onClick={() => setCurrentTab("ask")}>Ask</button>
-      </li>
-      <li
-        className={`${
-          currentTab === "jobs"
-            ? "text-blue-500"
-            : "text-gray-500 hover:text-gray-400"
-        } transition-all text-center font-bold  p-3 `}
-      >
-        <button onClick={() => setCurrentTab("jobs")}>Jobs</button>
-      </li>
+      {tabs.map(tab => (
+        <li
+          key={tab}
+          className="p-3 font-bold text-center transition-all rounded-lg"
+          style={{
+            backgroundColor:
+              currentTab === tab && settings
+                ? settings?.theme.text + 55
+                : "transparent",
+            color:
+              currentTab !== tab
+                ? settings?.theme.text
+                : settings?.theme.primary,
+          }}
+        >
+          <button className="capitalize" onClick={() => setCurrentTab(tab)}>
+            {tab}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 };
