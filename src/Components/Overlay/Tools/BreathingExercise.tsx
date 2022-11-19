38e435b98529260d9a8196296718611211035530
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-19 18:46:50 +0100

    improve animation

--- src/Components/Overlay/Tools/BreathingExercise.tsx
+++ src/Components/Overlay/Tools/BreathingExercise.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { AiOutlineSmile } from "react-icons/ai";
 
@@ -96,56 +96,67 @@ const BreathingExercise = () => {
         <AiOutlineSmile />
       </motion.button>
 
-      <motion.div
-        initial={{
-          // transform: "translate(-50%,-50%) scaleY(0)",
-          x: "-50%",
-          y: "-50%",
-          scaleY: 0,
-          scaleX: 0,
-        }}
-        animate={{
-          // transform: `translate(-50%,-50%) scaleY(${
-          //   showInfo ? 1 : 0.1
-          // }) scaleX(${showInfo ? 1 : 0})`,
-          x: "-50%",
-          y: "-50%",
-          scaleY: showInfo ? [0, 1, 1, 1] : [1, 0.005, 0],
-          scaleX: showInfo ? 1 : [1, 1, 1, 0.5, 0.1, 0],
-        }}
-        className="absolute w-[min(800px,100vw)] h-[90vh] font-[GreatVibes] font-light text-4xl overflow-scroll noscroll bg-[#1da0f254] z-20 flex flex-col gap-4 text-white p-4 rounded shadow-xl top-1/2 left-1/2 backdrop-blur-2xl"
-      >
-        <p>
-          • Anxiety is a normal, although unpleasant, part of life, and it can
-          affect us in different ways and at different times of the day. Whereas
-          stress is something that will come and go and it's all in how we
-          handle those stressful moments.
-        </p>
-        <p>
-          • This simple exercise can help ease anxiety and stop it becoming
-          overwhelming, however it's worth practicing for several minutes.
-        </p>
-        <p>
-          • Furthermore your breathing can contribute to your anxiety and panic,
-          most people aren't really conscious of the way they're breathing, but
-          generally, there are two types of breathing patterns:
-        </p>
-        <ul className="pl-4">
-          <li>1 - Thoracic (chest) breathing</li>
-          <li>2 - Diaphragmatic (abdominal) breathing</li>
-        </ul>
+      <AnimatePresence>
+        {showInfo && (
+          <motion.div
+            initial={{
+              x: "-50%",
+              y: "-50%",
+              scaleY: 0,
+              scaleX: 0,
+            }}
+            animate={{
+              x: "-50%",
+              y: "-50%",
+              scaleY: [0.01, 0.01, 1],
+              scaleX: 1,
+            }}
+            exit={{
+              x: "-50%",
+              y: "-50%",
+              scaleY: [1, 0.01, 0.01],
+              scaleX: [1, 1, 0.01],
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="absolute w-[min(800px,100vw)] h-[90vh] font-[GreatVibes] font-light text-4xl overflow-scroll noscroll bg-[#1da0f254] z-20 flex flex-col gap-4 text-white p-4 rounded shadow-xl top-1/2 left-1/2"
+          >
+            <p>
+              • Anxiety is a normal, although unpleasant, part of life, and it
+              can affect us in different ways and at different times of the day.
+              Whereas stress is something that will come and go and it's all in
+              how we handle those stressful moments.
+            </p>
+            <p>
+              • This simple exercise can help ease anxiety and stop it becoming
+              overwhelming, however it's worth practicing for several minutes.
+            </p>
+            <p>
+              • Furthermore your breathing can contribute to your anxiety and
+              panic, most people aren't really conscious of the way they're
+              breathing, but generally, there are two types of breathing
+              patterns:
+            </p>
+            <ul className="pl-4">
+              <li>1 - Thoracic (chest) breathing</li>
+              <li>2 - Diaphragmatic (abdominal) breathing</li>
+            </ul>
 
-        <p>
-          • Chest breathing causes an upset in the oxygen and carbon dioxide
-          levels in the body resulting in increased heart rate, dizziness,
-          muscle tension, and other physical sensations. Whereas during
-          abdominal or diaphragmatic breathing, you take even, deep breaths.
-        </p>
-        <p>
-          • This is actually the way newborn babies naturally breathe and you're
-          also probably using this kind of breathing when you're in deep sleep.
-        </p>
-      </motion.div>
+            <p>
+              • Chest breathing causes an upset in the oxygen and carbon dioxide
+              levels in the body resulting in increased heart rate, dizziness,
+              muscle tension, and other physical sensations. Whereas during
+              abdominal or diaphragmatic breathing, you take even, deep breaths.
+            </p>
+            <p>
+              • This is actually the way newborn babies naturally breathe and
+              you're also probably using this kind of breathing when you're in
+              deep sleep.
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
