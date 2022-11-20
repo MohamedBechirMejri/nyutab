8904b79d4d1e2350d6f9c1622648fea5685559dd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-20 14:31:30 +0100

    add getcoords

--- src/lib/locationUtils.ts
+++ src/lib/locationUtils.ts
@@ -7,3 +7,14 @@ export const getLocation = async (latitude: any, longitude: any) => {
   // TODO: look for a better location api
   return { city: res.data.location.name, country: res.data.location.country };
 };
+
+export const getCoords = async (city: string) => {
+  const res = await axios.get(
+    `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}`
+  );
+
+  return {
+    latitude: res.data.location.lat,
+    longitude: res.data.location.lon,
+  };
+};
