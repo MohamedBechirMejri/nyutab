8630f1d61643fc07217ad8123e7a99282c7366f3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-21 14:29:18 +0100

    remove redundant code

--- src/Components/Overlay/Games/Games.tsx
+++ src/Components/Overlay/Games/Games.tsx
@@ -4,7 +4,7 @@ const Games = () => {
   return (
     <div className="flex flex-col items-center w-full h-full p-2 overflow-y-scroll noscroll">
       <h1 className="text-xl font-semibold">Games</h1>
-      <div className="grid w-full h-max grid-cols-2 gap-4 p-4 grid-rows-[repeat(25,minmax(0,1fr))]">
+      <div className="grid w-full grid-cols-2 gap-4 p-4 h-max">
         <Button
           name="Game"
           soon={true}
