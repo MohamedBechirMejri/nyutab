271808a6c98d284f1112c9a5992a35f17ce22acb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-22 13:20:28 +0100

    add scrollbar for long text

--- src/Components/Overlay/Tools/Calculator/Calculator.tsx
+++ src/Components/Overlay/Tools/Calculator/Calculator.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Keypad from "./Keypad";
 
 const Calculator = () => {
+  const [expression, setExpression] = useState("");
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div className="grid grid-rows-[1fr,3fr] max-h-screen overflow-hidden border-4 border-[#2b424677] shadow-xl rounded-xl w-[min(30rem,95vw)] h-[min(50rem,95vh)]">
-        <div className="bg-[#253438] bg-opacity-60 backdrop-blur-3xl"></div>
-        <Keypad />
+    <div className="flex items-center justify-center w-full h-full font-[FiraCode]">
+      <div className="grid grid-rows-[1fr,3fr] overflow-hidden shadow-3xl rounded-xl w-[min(30rem,95vw)] h-[min(50rem,95vh)] border border-[#2b424677] bg-[#171b1c]">
+        <div className="bg-[#253438] bg-opacity-60 backdrop-blur-3xl p-4 shadow-xl rounded-lg overflow-x-scroll selection:bg-[#34717c] w-full scroll">
+          <p className="w-max"> {expression}</p>
+        </div>
+        <Keypad setExpression={setExpression} />
       </div>
     </div>
   );
