6d3a0b4f684055ae6a913627977f181ccda62ec7
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-22 12:12:39 +0100

    tweak width

--- src/Components/Overlay/Tools/Calculator/Calculator.tsx
+++ src/Components/Overlay/Tools/Calculator/Calculator.tsx
@@ -3,7 +3,7 @@ import Keypad from "./Keypad";
 const Calculator = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="grid grid-rows-[1fr,4fr] max-h-screen overflow-hidden border-4 rounded-xl w-[min(33rem,95vw)] h-[min(50rem,95vh)]">
+      <div className="grid grid-rows-[1fr,3fr] max-h-screen overflow-hidden border-4 border-[#2b424677] shadow-xl rounded-xl w-[min(30rem,95vw)] h-[min(50rem,95vh)]">
         <div className="bg-[#253438] bg-opacity-60 backdrop-blur-3xl"></div>
         <Keypad />
       </div>
