20c63f71d937e638e75b11007430daedb09984b6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-22 21:51:40 +0100

    Add todos

--- src/Components/Overlay/Tools/Calculator/Calculator.tsx
+++ src/Components/Overlay/Tools/Calculator/Calculator.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { compile } from "mathjs";
 import Keypad from "./Keypad";
 
+// TODO: Add more expression options () sin sqrt...
+// TODO: Add syntax checker
+
 const Calculator = () => {
+  const resultRef = useRef(null);
+
   const [expression, setExpression] = useState("");
   const [result, setResult] = useState(0);
 
@@ -10,21 +15,26 @@ const Calculator = () => {
     const lastChar = expression.charAt(expression.length - 1);
     if (!/%|\/|\*|^-|\+/.test(lastChar))
       setResult(compile(expression).evaluate());
+
+    // @ts-ignore
+    resultRef.current.scrollLeft = resultRef.current.scrollWidth;
   }, [expression]);
 
   return (
     <div className="flex items-center justify-center w-full h-full font-[FiraCode]">
       <div className="grid grid-rows-[1fr,3fr] overflow-hidden shadow-3xl rounded-xl w-[min(30rem,95vw)] h-[min(50rem,95vh)] border border-[#2b424677] bg-[#171b1c]">
-        <div className="bg-[#253438] bg-opacity-60 backdrop-blur-3xl p-4 shadow-xl rounded-lg overflow-x-scroll selection:bg-[#34717c] w-full scroll">
-          <p className="w-max">
-            {" "}
+        <div
+          ref={resultRef}
+          className="bg-[#253438] bg-opacity-60 backdrop-blur-3xl p-4 shadow-xl rounded-lg overflow-x-scroll selection:bg-[#34717c] w-full scroll grid grid-rows-2 text-right text-3xl items-end"
+        >
+          <p className="min-w-full w-max text-[#cbe7ee] text-5xl"> {result}</p>
+          <p className="min-w-full w-max">
             {expression
               .replaceAll("*", " × ")
               .replaceAll("/", " / ")
               .replaceAll("+", " + ")
               .replaceAll("-", " - ")}
           </p>
-          <p className="w-max"> {result}</p>
         </div>
         <Keypad setExpression={setExpression} />
       </div>
