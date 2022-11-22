4aa4921e4ef8e5d24a725f26e0f75917c6c3f309
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-22 21:20:38 +0100

    add basic calculation ability

--- src/Components/Overlay/Tools/Calculator/Calculator.tsx
+++ src/Components/Overlay/Tools/Calculator/Calculator.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { compile } from "mathjs";
 import Keypad from "./Keypad";
 
 const Calculator = () => {
   const [expression, setExpression] = useState("");
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    const lastChar = expression.charAt(expression.length - 1);
+    if (!/%|\/|\*|^-|\+/.test(lastChar))
+      setResult(compile(expression).evaluate());
+  }, [expression]);
 
   return (
     <div className="flex items-center justify-center w-full h-full font-[FiraCode]">
       <div className="grid grid-rows-[1fr,3fr] overflow-hidden shadow-3xl rounded-xl w-[min(30rem,95vw)] h-[min(50rem,95vh)] border border-[#2b424677] bg-[#171b1c]">
         <div className="bg-[#253438] bg-opacity-60 backdrop-blur-3xl p-4 shadow-xl rounded-lg overflow-x-scroll selection:bg-[#34717c] w-full scroll">
-          <p className="w-max"> {expression}</p>
+          <p className="w-max">
+            {" "}
+            {expression
+              .replaceAll("*", " × ")
+              .replaceAll("/", " / ")
+              .replaceAll("+", " + ")
+              .replaceAll("-", " - ")}
+          </p>
+          <p className="w-max"> {result}</p>
         </div>
         <Keypad setExpression={setExpression} />
       </div>
