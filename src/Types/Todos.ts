8ff37e7facbdcd161956b193e56e27aedf962c41
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-24 23:36:22 +0100

    add ids

--- src/Types/Todos.ts
+++ src/Types/Todos.ts
@@ -1,14 +1,17 @@
 export type Subtask = {
+  id: any;
   text: string;
   isCompleted: boolean;
 };
 export type Task = {
+  id: any;
   title: string;
   isCompleted: boolean;
   showSubtasks: boolean;
   subtasks: Subtask[];
 };
 type Project = {
+  id: any;
   title: string;
   tasks: Task[];
 };
