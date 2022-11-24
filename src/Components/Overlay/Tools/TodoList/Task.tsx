6412bb9a4df95b1a2f4765b4928825044d094379
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-24 23:35:53 +0100

    tweak buttons

--- src/Components/Overlay/Tools/TodoList/Task.tsx
+++ src/Components/Overlay/Tools/TodoList/Task.tsx
@@ -5,17 +5,6 @@ import { TiDeleteOutline, TiPlusOutline } from "react-icons/ti";
 import Project from "../../../../Types/Todos";
 import Subtask from "./SubTask";
 
-const buttonAnimation = {
-  initial: { opacity: 1, x: -54, scale: 0 },
-  animate: {
-    opacity: 1,
-    x: 0,
-    scale: 1,
-  },
-  whileHover: { scale: 1.25 },
-  whileTap: { scale: 1 },
-};
-
 const Task = ({
   task,
   id,
@@ -26,6 +15,18 @@ const Task = ({
   setProjects: any;
 }) => {
   const [subtasks, setSubtasks] = useState(task.subtasks);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const buttonAnimation = {
+    initial: { opacity: 0, x: -15, scale: 0 },
+    animate: {
+      opacity: isHovered ? 1 : 0,
+      x: isHovered ? 0 : -15,
+      scale: isHovered ? 1.1 : 0,
+    },
+    whileHover: { scale: 1.25 },
+    whileTap: { scale: 1 },
+  };
 
   const handleAdd = () => {
     //   setSubtasks([{ isCompleted: false, text: "" }, ...subtasks]);
@@ -37,9 +38,12 @@ const Task = ({
   };
 
   return (
-    <div key={"task-" + id}>
-      <div className="flex items-center gap-4 pb-4 text-2xl">
-        <h2> {task.title}</h2>
+    <div key={task.id}>
+      <div
+        className="flex items-center gap-4 pb-4 -ml-32 text-2xl"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <div className="flex gap-4">
           <motion.button {...buttonAnimation} onClick={handleAdd}>
             <TiPlusOutline className="text-green-500" />
@@ -56,7 +60,15 @@ const Task = ({
           >
             <MdOutlineCheckCircle className="text-blue-500" />
           </motion.button>
-        </div>
+        </div>{" "}
+        <input
+          type="text"
+          value={task.title}
+          placeholder="What should I do next?"
+          className={`w-full bg-transparent border-none outline-none focus:ring-0 ${
+            task.isCompleted ? `line-through text-gray-500` : ""
+          }`}
+        />
       </div>
       <ul className="flex flex-col gap-4">
         {task.subtasks.map((subtask: any, subtaskIndex: any) => (
