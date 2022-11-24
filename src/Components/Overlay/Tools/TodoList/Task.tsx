097a21504b37250476e137c129c3ce42c5544afc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-24 21:57:26 +0100

    add new task handler

--- src/Components/Overlay/Tools/TodoList/Task.tsx
+++ src/Components/Overlay/Tools/TodoList/Task.tsx
@@ -1,7 +1,20 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import { MdOutlineCheckCircle } from "react-icons/md";
 import { TiDeleteOutline, TiPlusOutline } from "react-icons/ti";
+import Project from "../../../../Types/Todos";
 import Subtask from "./SubTask";
+
+const buttonAnimation = {
+  initial: { opacity: 1, x: -54, scale: 0 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    scale: 1,
+  },
+  whileHover: { scale: 1.25 },
+  whileTap: { scale: 1 },
+};
 
 const Task = ({
   task,
@@ -14,25 +27,23 @@ const Task = ({
   currentProject: number;
   setProjects: any;
 }) => {
-  const buttonAnimation = {
-    initial: { opacity: 1, x: -54, scale: 0 },
-    animate: {
-      opacity: 1,
-      x: 0,
-      scale: 1,
-    },
-    whileHover: { scale: 1.25 },
-    whileTap: { scale: 1 },
+  const [subtasks, setSubtasks] = useState(task.subtasks);
+
+  const handleAdd = () => {
+    setSubtasks([{ isCompleted: false, text: "" }, ...subtasks]);
+    setProjects((projects: Project[]) => {
+      const newProjects = projects;
+      newProjects[currentProject].tasks[id].subtasks = subtasks;
+      return [...newProjects];
+    });
   };
+
   return (
     <div key={"task-" + id}>
       <div className="flex items-center gap-4 pb-4 text-2xl">
         <h2> {task.title}</h2>
         <div className="flex gap-4">
-          <motion.button
-            {...buttonAnimation}
-            // onClick={handleDelete}
-          >
+          <motion.button {...buttonAnimation} onClick={handleAdd}>
             <TiPlusOutline className="text-green-500" />
           </motion.button>
           <motion.button
