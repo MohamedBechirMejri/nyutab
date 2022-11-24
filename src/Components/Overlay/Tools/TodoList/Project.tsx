9be703e0036dd56a261421d0d4fa9c31300bf683
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-24 23:36:11 +0100

    add add task handler

--- src/Components/Overlay/Tools/TodoList/Project.tsx
+++ src/Components/Overlay/Tools/TodoList/Project.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import uniqid from "uniqid";
 import type ProjectType from "../../../../Types/Todos";
 import Task from "./Task";
 
@@ -26,8 +27,21 @@ const Project = ({
     });
   };
 
+  const addTask = () => {
+    setTasks([
+      {
+        id: uniqid(),
+        title: "",
+        isCompleted: false,
+        showSubtasks: false,
+        subtasks: [],
+      },
+      ...tasks,
+    ]);
+  };
+
   return (
-    <div className="flex flex-col items-start gap-4">
+    <div className="flex flex-col items-start gap-4 pl-16">
       <input
         className="text-4xl bg-transparent outline-none w-max"
         placeholder="Untitled"
@@ -36,9 +50,14 @@ const Project = ({
         onChange={handleChange}
       />
 
-      <button>New Task</button>
+      <button
+        className="p-2 px-6 font-medium text-orange-500 transition-all bg-orange-500 bg-opacity-25 rounded backdrop-blur-3xl active:scale-95"
+        onClick={addTask}
+      >
+        New Task
+      </button>
 
-      {/* <div className="flex flex-col w-full gap-4 text-xl">
+      <div className="flex flex-col w-full gap-4 text-xl">
         {tasks.map((task, taskIndex) => (
           <Task
             key={"Task-" + taskIndex}
@@ -47,7 +66,7 @@ const Project = ({
             setProjects={setProjects}
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
