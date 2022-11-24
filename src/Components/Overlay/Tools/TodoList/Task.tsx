2ed481de7f8b8e67bc29673ded89a5f28807ce6e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-24 21:07:19 +0100

    style buttons

--- src/Components/Overlay/Tools/TodoList/Task.tsx
+++ src/Components/Overlay/Tools/TodoList/Task.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { MdOutlineCheckCircle } from "react-icons/md";
+import { TiDeleteOutline, TiPlusOutline } from "react-icons/ti";
 import Subtask from "./SubTask";
 
 const Task = ({
@@ -11,14 +14,39 @@ const Task = ({
   currentProject: number;
   setProjects: any;
 }) => {
+  const buttonAnimation = {
+    initial: { opacity: 1, x: -54, scale: 0 },
+    animate: {
+      opacity: 1,
+      x: 0,
+      scale: 1,
+    },
+    whileHover: { scale: 1.25 },
+    whileTap: { scale: 1 },
+  };
   return (
     <div key={"task-" + id}>
-      <div className="flex gap-4 pb-4 text-2xl">
+      <div className="flex items-center gap-4 pb-4 text-2xl">
         <h2> {task.title}</h2>
         <div className="flex gap-4">
-          <button>+</button>
-          <button>-</button>
-          <button>x</button>
+          <motion.button
+            {...buttonAnimation}
+            // onClick={handleDelete}
+          >
+            <TiPlusOutline className="text-green-500" />
+          </motion.button>
+          <motion.button
+            {...buttonAnimation}
+            // onClick={handleDelete}
+          >
+            <TiDeleteOutline className="text-red-500" />
+          </motion.button>
+          <motion.button
+            {...buttonAnimation}
+            // onClick={handleToggle}
+          >
+            <MdOutlineCheckCircle className="text-blue-500" />
+          </motion.button>
         </div>
       </div>
       <ul className="flex flex-col gap-4">
