16504fb02a6183dcfa27b525bff25d463b250201
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-25 18:38:07 +0100

    Add games placeholders

--- src/Components/Overlay/Games/Games.tsx
+++ src/Components/Overlay/Games/Games.tsx
@@ -6,9 +6,27 @@ const Games = () => {
       <h1 className="text-xl font-semibold">Games</h1>
       <div className="grid w-full grid-cols-2 gap-4 p-4 h-max">
         <Button
-          name="Game"
+          name="Guess the word"
           soon={true}
-          className="text-[#f43f5e] hover:bg-[#f43f5e27] bg-[#f43f5e11]"
+          className="text-[#84fff7] hover:bg-[#84fff727] bg-[#84fff711]"
+          handleClick={() => console.log("prayers")}
+        />
+        <Button
+          name="Aim Bot"
+          soon={true}
+          className="text-[#eab308] hover:bg-[#eab30827] bg-[#eab30811]"
+          handleClick={() => console.log("prayers")}
+        />
+        <Button
+          name="Battleship"
+          soon={true}
+          className="text-[#EF8275] hover:bg-[#EF827527] bg-[#EF827511]"
+          handleClick={() => console.log("prayers")}
+        />
+        <Button
+          name="Daily Puzzle"
+          soon={true}
+          className="text-[#BCD0C7] hover:bg-[#BCD0C727] bg-[#BCD0C711]"
           handleClick={() => console.log("prayers")}
         />
       </div>
