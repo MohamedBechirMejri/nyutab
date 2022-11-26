35637b4f22e63ae59cb87aff07cdb3e30512066c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-26 01:57:21 +0100

    improve performance

--- src/Components/Overlay/Tools/Awesome.tsx
+++ src/Components/Overlay/Tools/Awesome.tsx
@@ -1,15 +1,36 @@
 import AwesomeData from "../../../db/AwesomeData.json";
 import { decompress } from "compress-json";
+import { useEffect, useState } from "react";
 
 // TODO: Test Memoization
 
 const Awesome = () => {
-  // @ts-ignore
-  const data = decompress(AwesomeData);
+  const [categories, setCategories] = useState<any>(null);
+  const [category, setCategory] = useState<any>(null);
 
-  console.log(data);
+  useEffect(() => {
+    // @ts-ignore
+    const data = decompress(AwesomeData);
+    const categories = Object.entries(data);
+    console.log(categories);
+    setCategories(categories);
+    setCategory(categories[0]);
+  }, []);
 
-  return <div></div>;
+  return categories ? (
+    <div className="grid w-full h-full grid-cols-6 grid-rows-1">
+      <div className="flex flex-col h-full gap-2 overflow-scroll noscroll">
+        {categories.map(([name, lists]: [name: string, lists: any]) => {
+          return (
+            <button onClick={() => setCategory([name, lists])}>{name}</button>
+          );
+        })}
+      </div>
+      <div>
+        <h1>{category[0]}</h1>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Awesome;
