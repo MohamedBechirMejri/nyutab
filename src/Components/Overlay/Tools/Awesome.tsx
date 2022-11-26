9634b69d918c3be7e8f3ea47827511b2ac39b3d1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-26 03:08:26 +0100

    show awesome links

--- src/Components/Overlay/Tools/Awesome.tsx
+++ src/Components/Overlay/Tools/Awesome.tsx
@@ -3,7 +3,7 @@ import { decompress } from "compress-json";
 import { useEffect, useState } from "react";
 import { getRandomNumber } from "../../../lib/mathUtils";
 
-// TODO: Test Memoization
+// TODO: Filter Broken links, Improve Look, Fix Slow Performance, Add random site button
 
 const Awesome = () => {
   const [categories, setCategories] = useState<any>(null);
@@ -19,16 +19,43 @@ const Awesome = () => {
   }, []);
 
   return categories ? (
-    <div className="grid w-full h-full grid-cols-6 grid-rows-1">
-      <div className="flex flex-col h-full gap-2 overflow-scroll noscroll">
-        {categories.map(([name, lists]: [name: string, lists: any]) => {
+    <div className="grid w-full h-full grid-cols-6 grid-rows-1 p-8 rounded-lg bg-slate-800">
+      <div className="flex flex-col h-full gap-4 p-4 overflow-scroll noscroll">
+        {categories.map(([name]: [name: string], i: number) => {
           return (
-            <button onClick={() => setCategory([name, lists])}>{name}</button>
+            <button key={name} onClick={() => setCategory(categories[i])}>
+              {name}
+            </button>
           );
         })}
       </div>
-      <div>
-        <h1>{category[0]}</h1>
+      <div className="flex flex-col w-full h-full col-span-5 gap-2 p-8 overflow-scroll rounded-lg noscroll">
+        <h1 className="text-4xl font-semibold">{category[0]}</h1>
+        <div>
+          {category[1].map((list: any) => {
+            return (
+              <div>
+                <h2 className="m-4 text-3xl">- {list.name}</h2>
+                <div className="pl-8">
+                  {list.repo.map((link: any) => (
+                    <div className="flex w-full gap-2 my-2 text-xl">
+                      <span>•</span>
+                      <a
+                        href={link.url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-blue-400 shrink-0"
+                      >
+                        {link.name}
+                      </a>
+                      {link.description && <p>- {link.description}</p>}.
+                    </div>
+                  ))}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   ) : null;
