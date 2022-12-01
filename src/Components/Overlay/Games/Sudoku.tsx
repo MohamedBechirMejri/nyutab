e83e325a9206820fedd5bc0f5b75922e286d33a0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-01 22:48:35 +0100

    improve borders

--- src/Components/Overlay/Games/Sudoku.tsx
+++ src/Components/Overlay/Games/Sudoku.tsx
@@ -6,28 +6,47 @@ const Sudoku = () => {
 
   return (
     <div className="flex items-center justify-center h-full font-[FiraCode]">
-      <div className="w-[35rem] h-[35rem] grid grid-cols-9 border font-bold rounded-xl overflow-hidden bg-gradient-to-br from-white to-gray-300 text-black shadow-2xl p-1 grid-rows-[repeat(9,minmax(0,1fr))] ">
+      <div className="w-[35rem] h-[35rem] grid grid-cols-9 font-bold rounded-xl overflow-hidden bg-gradient-to-br from-white to-gray-200 text-black shadow-2xl grid-rows-[repeat(9,minmax(0,1fr))] border">
         {puzzle.map((zone: any, i: number) => {
           return (
             <div
               key={"zone-" + i}
               className="flex items-center justify-center border"
               style={{
-                borderBottomColor:
-                  (i >= 18 && i <= 26) || (i >= 45 && i <= 53)
-                    ? "black"
-                    : "transparent",
+                background:
+                  zone !== null
+                    ? "#ddd"
+                    : // : (!((i + 6) % 9) ||
+                      //     !((i + 5) % 9) ||
+                      //     !((i + 4) % 9) ||
+                      //     (i >= 27 && i <= 53)) &&
+                      //   i !== 30 &&
+                      //   i !== 31 &&
+                      //   i !== 32 &&
+                      //   i !== 39 &&
+                      //   i !== 40 &&
+                      //   i !== 41 &&
+                      //   i !== 48 &&
+                      //   i !== 49 &&
+                      //   i !== 50
+                      // ? "#9977ff22"
+                      "transparent",
                 borderTopColor:
-                  (i >= 27 && i <= 35) || (i >= 54 && i <= 62)
-                    ? "black"
-                    : "transparent",
+                  (i >= 54 && i <= 62) || (i >= 27 && i <= 35)
+                    ? "#999"
+                    : "#ccc",
                 borderRightColor:
-                  !((i + 7) % 9) || !((i + 4) % 9) ? "black" : "transparent",
+                  !((i + 7) % 9) || !((i + 4) % 9) ? "#999" : "#ccc",
+                borderBottomColor:
+                  (i >= 45 && i <= 53) || (i >= 18 && i <= 26)
+                    ? "#999"
+                    : "#ccc",
                 borderLeftColor:
-                  !((i + 6) % 9) || !((i + 3) % 9) ? "black" : "transparent",
+                  !((i + 6) % 9) || !((i + 3) % 9) ? "#999" : "#ccc",
               }}
             >
-              {i}
+              {zone !== null && zone + 1}
+              {/* {i} */}
             </div>
           );
         })}
