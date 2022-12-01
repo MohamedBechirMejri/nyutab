cf2ce9ccd13e1b9d18ea2759f2d193055d9e5c04
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-01 22:53:17 +0100

    clean code

--- src/Components/Overlay/Games/Sudoku.tsx
+++ src/Components/Overlay/Games/Sudoku.tsx
@@ -8,24 +8,21 @@ const Sudoku = () => {
     <div className="flex items-center justify-center h-full font-[FiraCode]">
       <div className="w-[35rem] h-[35rem] grid grid-cols-9 font-bold rounded-xl overflow-hidden bg-gradient-to-br from-white to-gray-200 text-black shadow-2xl grid-rows-[repeat(9,minmax(0,1fr))] border">
         {puzzle.map((zone: any, i: number) => {
+          const showTopBorder = (i >= 54 && i <= 62) || (i >= 27 && i <= 35);
+          const showBottomBorder = (i >= 45 && i <= 53) || (i >= 18 && i <= 26);
+          const showRightBorder = !((i + 7) % 9) || !((i + 4) % 9);
+          const showLeftBorder = !((i + 6) % 9) || !((i + 3) % 9);
+
           return (
             <div
               key={"zone-" + i}
               className="flex items-center justify-center border"
               style={{
                 background: zone !== null ? "#ddd" : "transparent",
-                borderTopColor:
-                  (i >= 54 && i <= 62) || (i >= 27 && i <= 35)
-                    ? "#999"
-                    : "#ccc",
-                borderRightColor:
-                  !((i + 7) % 9) || !((i + 4) % 9) ? "#999" : "#ccc",
-                borderBottomColor:
-                  (i >= 45 && i <= 53) || (i >= 18 && i <= 26)
-                    ? "#999"
-                    : "#ccc",
-                borderLeftColor:
-                  !((i + 6) % 9) || !((i + 3) % 9) ? "#999" : "#ccc",
+                borderTopColor: showTopBorder ? "#999" : "#ccc",
+                borderBottomColor: showBottomBorder ? "#999" : "#ccc",
+                borderRightColor: showRightBorder ? "#999" : "#ccc",
+                borderLeftColor: showLeftBorder ? "#999" : "#ccc",
               }}
             >
               {zone !== null && zone + 1}
