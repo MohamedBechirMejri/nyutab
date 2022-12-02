b5f5a1a18a9c1975fb8076d415064ab5c911378f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-02 03:44:23 +0100

    Add Basic Functionality

--- src/Components/Overlay/Games/Sudoku.tsx
+++ src/Components/Overlay/Games/Sudoku.tsx
@@ -1,36 +1,86 @@
-import { useState } from "react";
-import { makepuzzle, solvepuzzle, ratepuzzle } from "sudoku";
+import { useEffect, useState } from "react";
+import { makepuzzle, solvepuzzle } from "sudoku";
+import uniqid from "uniqid";
 
 const Sudoku = () => {
-  const [puzzle, setPuzzle] = useState(makepuzzle());
+  const [initialPuzzle, setInitialPuzzle] = useState<number[] | null>(null);
+  const [solvedPuzzle, setSolvedPuzzle] = useState<number[] | null>(null);
+  const [puzzle, setPuzzle] = useState<number[] | null>(null);
+
+  useEffect(() => {
+    // load current game from localstorage
+    setInitialPuzzle(makepuzzle());
+  }, []);
+
+  useEffect(() => {
+    setPuzzle(initialPuzzle);
+    setSolvedPuzzle(solvepuzzle(initialPuzzle));
+  }, [initialPuzzle]);
 
   return (
     <div className="flex items-center justify-center h-full font-[FiraCode]">
-      <div className="w-[35rem] h-[35rem] grid grid-cols-9 font-bold rounded-xl overflow-hidden bg-gradient-to-br from-white to-gray-200 text-black shadow-2xl grid-rows-[repeat(9,minmax(0,1fr))] border">
-        {puzzle.map((zone: any, i: number) => {
-          const showTopBorder = (i >= 54 && i <= 62) || (i >= 27 && i <= 35);
-          const showBottomBorder = (i >= 45 && i <= 53) || (i >= 18 && i <= 26);
-          const showRightBorder = !((i + 7) % 9) || !((i + 4) % 9);
-          const showLeftBorder = !((i + 6) % 9) || !((i + 3) % 9);
+      <div className="w-[33rem] h-[34rem] grid grid-cols-9 font-bold text-black shadow-2xl grid-rows-[repeat(9,minmax(0,1fr))] ">
+        {puzzle &&
+          puzzle.map((field: any, i: number) => {
+            const showTopBorder = (i >= 54 && i <= 62) || (i >= 27 && i <= 35);
+            const showBottomBorder =
+              (i >= 45 && i <= 53) || (i >= 18 && i <= 26);
+            const showRightBorder = !((i + 7) % 9) || !((i + 4) % 9);
+            const showLeftBorder = !((i + 6) % 9) || !((i + 3) % 9);
 
-          return (
-            <div
-              key={"zone-" + i}
-              className="flex items-center justify-center border"
-              style={{
-                background: zone !== null ? "#ddd" : "transparent",
-                borderTopColor: showTopBorder ? "#999" : "#ccc",
-                borderBottomColor: showBottomBorder ? "#999" : "#ccc",
-                borderRightColor: showRightBorder ? "#999" : "#ccc",
-                borderLeftColor: showLeftBorder ? "#999" : "#ccc",
-              }}
-            >
-              {zone !== null && zone + 1}
-              {/* {i} */}
-            </div>
-          );
-        })}
+            return (
+              <div
+                key={"field-" + i}
+                className={`relative flex items-center justify-center transition-all border group from-white to-gray-200 ${
+                  initialPuzzle![i] !== null
+                    ? "bg-gray-300"
+                    : "bg-gradient-to-br"
+                } `}
+                style={{
+                  borderTopColor: showTopBorder ? "#999" : "#ccc",
+                  borderBottomColor: showBottomBorder ? "#999" : "#ccc",
+                  borderRightColor: showRightBorder ? "#999" : "#ccc",
+                  borderLeftColor: showLeftBorder ? "#999" : "#ccc",
+                  borderTopLeftRadius: i === 0 ? "1rem" : 0,
+                  borderTopRightRadius: i === 8 ? "1rem" : 0,
+                  borderBottomLeftRadius: i === 72 ? "1rem" : 0,
+                  borderBottomRightRadius: i === 80 ? "1rem" : 0,
+                }}
+              >
+                <p>{field !== null && field + 1}</p>
+
+                {initialPuzzle![i] === null ? (
+                  <div className="absolute z-10 grid w-[5rem] h-[5rem] grid-cols-3 overflow-hidden transition-all scale-0 -translate-x-1/2 -translate-y-1/2 border shadow-2xl top-1/2 left-1/2 bg-gradient-to-bl from-gray-200 to-white rounded-xl group-hover:scale-100 group-hover:opacity-100 opacity-0 select-none duration-300 border-black">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(n => {
+                      return (
+                        <button
+                          key={uniqid()}
+                          className={`flex items-center justify-center transition-all hover:bg-gray-300 ${
+                            n - 1 === field ? "text-blue-500" : ""
+                          } `}
+                          onClick={() =>
+                            setPuzzle((puzzle: any) => {
+                              const newPuzzle = [...puzzle];
+                              newPuzzle[i] = solvedPuzzle![i]; // +n - 1;
+                              return [...newPuzzle];
+                            })
+                          }
+                        >
+                          {n}
+                        </button>
+                      );
+                    })}
+                  </div>
+                ) : null}
+              </div>
+            );
+          })}
       </div>
+      {/* <h1>
+        {JSON.stringify(puzzle) === JSON.stringify(solvedPuzzle)
+          ? "solved!"
+          : "not solved"}
+      </h1> */}
     </div>
   );
 };
