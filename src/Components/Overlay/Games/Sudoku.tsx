cfd8dc43633a550a11cb9398cdddce8efaf56bb4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-02 06:58:13 +0100

    Add Timer and buttons

--- src/Components/Overlay/Games/Sudoku.tsx
+++ src/Components/Overlay/Games/Sudoku.tsx
@@ -18,15 +18,39 @@ const Sudoku = () => {
   }, [initialPuzzle]);
 
   return (
-    <div className="flex items-center justify-center h-full font-[FiraCode]">
-      {puzzle && (
-        <Board
-          puzzle={puzzle}
-          initialPuzzle={initialPuzzle}
-          setPuzzle={setPuzzle}
-        />
-      )}
+    <div className="grid grid-cols-3 h-full font-[FiraCode]">
+      <div></div>
+      <div className="flex items-center justify-center h-full">
+        {puzzle && (
+          <Board
+            puzzle={puzzle}
+            initialPuzzle={initialPuzzle}
+            setPuzzle={setPuzzle}
+          />
+        )}
+      </div>
       {/*  {JSON.stringify(puzzle) === JSON.stringify(solvedPuzzle)  */}
+      <div className="flex items-center justify-center h-full select-none">
+        <div className="grid grid-cols-12 min-h-[33rem] w-full px-16 gap-4">
+          <h1 className="flex items-center justify-center w-full col-span-12 py-4 text-4xl font-bold">
+            00:00:00
+          </h1>
+          <button className="col-span-10 transition-all bg-green-500 shadow-xl rounded-xl active:scale-95">
+            New Game
+          </button>{" "}
+          <div className="col-span-2 transition-all bg-yellow-500 shadow-xl rounded-xl"></div>
+          <button className="col-span-8 transition-all bg-red-500 shadow-xl rounded-xl active:scale-95">
+            Check Errors
+          </button>
+          <button className="col-span-4 transition-all bg-orange-500 shadow-xl rounded-xl active:scale-95">
+            Hint
+          </button>
+          <div className="col-span-2 transition-all bg-blue-500 shadow-xl rounded-xl"></div>
+          <button className="col-span-10 transition-all bg-gray-500 shadow-xl rounded-xl active:scale-95">
+            Clear
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
