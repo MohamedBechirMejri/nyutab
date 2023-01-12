952cb5af961552927e00bd5364fd48146825de36
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-12 03:03:51 +0100

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { outDir: "build" },
+  server: { port: 3000 },
+});
