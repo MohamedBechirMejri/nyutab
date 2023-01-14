ca84acbcbebef1521849e7131f147b30dbe019ce
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-14 08:01:24 +0100

    add workaround if the tab is an object

--- src/Components/Misc/Nav.tsx
+++ src/Components/Misc/Nav.tsx
@@ -6,37 +6,40 @@ const Nav = ({
   tab,
   setTab,
 }: {
-  tabs: string[];
+  tabs: any[];
   theme: { primary: string; text: string };
-  tab: string;
-  setTab: (tab: string) => void;
+  tab: any;
+  setTab: (tab: any) => void;
 }) => {
   return (
     <div className="flex items-center gap-8">
-      {tabs.map((t, i) => (
-        <motion.button
-          key={`${t}-${i}`}
-          initial={{
-            backgroundColor: "transparent",
-            borderColor: theme.primary,
-            color: theme.text,
-            borderRadius: "2rem",
-            scale: 0,
-          }}
-          animate={{
-            backgroundColor: tab === t ? theme.primary : theme.primary + 33,
-            borderColor: theme.primary,
-            color: theme.text,
-            scale: 1,
-          }}
-          whileHover={{ borderRadius: "1.5rem" }}
-          whileTap={{ borderRadius: "2rem", scale: 0.95 }}
-          className="w-full p-4 px-6 text-lg font-bold uppercase border"
-          onClick={() => setTab(t)}
-        >
-          {t}
-        </motion.button>
-      ))}
+      {tabs.map((t, i) => {
+        const name = typeof t === "string" ? t : t.name;
+        return (
+          <motion.button
+            key={`${name}-${i}`}
+            initial={{
+              backgroundColor: "transparent",
+              borderColor: theme.primary,
+              color: theme.text,
+              borderRadius: "2rem",
+              scale: 0,
+            }}
+            animate={{
+              backgroundColor: tab === t ? theme.primary : theme.primary + 33,
+              borderColor: theme.primary,
+              color: theme.text,
+              scale: 1,
+            }}
+            whileHover={{ borderRadius: "1.5rem" }}
+            whileTap={{ borderRadius: "2rem", scale: 0.95 }}
+            className="w-full p-4 px-6 text-lg font-bold uppercase border"
+            onClick={() => setTab(t)}
+          >
+            {name}
+          </motion.button>
+        );
+      })}
     </div>
   );
 };
