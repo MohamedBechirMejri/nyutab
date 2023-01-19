fc9e70e94d1e083d7846ea3ba148ef655d55cc3c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-19 03:00:15 +0100

    recreate component

--- src/Components/Overlay/Games/Minesweeper/Minesweeper.tsx
+++ src/Components/Overlay/Games/Minesweeper/Minesweeper.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
-const Minesweeper = () => {
-  const [board, setBoard] = useState([
+const addCoords = (
+  arr: string[][] = [
     ["o", "o", "1", "x", "3", "x", "2", "x", "2"],
     ["1", "1", "1", "2", "x", "2", "2", "2", "x"],
     ["x", "1", "o", "1", "1", "1", "o", "1", "1"],
@@ -12,90 +13,118 @@ const Minesweeper = () => {
     ["x", "x", "2", "2", "1", "1", "o", "1", "1"],
     ["x", "4", "x", "1", "o", "o", "o", "1", "x"],
     ["1", "2", "1", "1", "o", "o", "o", "1", "1"],
-  ]);
-  const [clicked, setClicked] = useState(
-    Array(board.length).fill(Array(board[0].length).fill(false))
-  );
+  ]
+) =>
+  arr
+    .map((row, y) =>
+      row.map((col, x) => ({ x, y, value: col, clicked: false }))
+    )
+    .flat();
 
+const Minesweeper = () => {
+  const [board, setBoard] = useState(addCoords());
+  const [width, setWidth] = useState(9);
+  const [height, setHeight] = useState(9);
   const [gameOver, setGameOver] = useState(false);
   const [gameWon, setGameWon] = useState(false);
+  const [time, setTime] = useState(0);
 
-  const getBoard = async () => {
-    const res = await axios.get(
-      "https://shadify.dev/api/minesweeper/generator?start=1-2"
-    );
-    setBoard(res.data.board);
+  const getNeighbors = (x: number, y: number) => {
+    const w = width - 1;
+    const h = height - 1;
+    const neighbors = [];
+    if (x > 0) neighbors.push({ x: x - 1, y });
+    if (x < w) neighbors.push({ x: x + 1, y });
+    if (y > 0) neighbors.push({ x, y: y - 1 });
+    if (y < h) neighbors.push({ x, y: y + 1 });
+    if (x > 0 && y > 0) neighbors.push({ x: x - 1, y: y - 1 });
+    if (x < w && y < h) neighbors.push({ x: x + 1, y: y + 1 });
+    if (x > 0 && y < h) neighbors.push({ x: x - 1, y: y + 1 });
+    if (x < w && y > 0) neighbors.push({ x: x + 1, y: y - 1 });
+    return neighbors;
   };
 
-  const reveal = (i: number, j: number) => {
-    setClicked(clicked => {
-      const newClicked = [...clicked];
-      const row = [...newClicked[i]];
-      row[j] = true;
-      newClicked[i] = row;
-      return [...newClicked];
-    });
-  };
-
-  const handleClick = (i: number, j: number) => {
-    if (
-      i < 0 ||
-      i >= board.length ||
-      j < 0 ||
-      j >= board[0].length ||
-      clicked[i][j] ||
-      gameOver ||
-      gameWon
-    )
+  const handleClick = (x: number, y: number) => {
+    if (gameOver) return;
+    const newBoard = [...board];
+    const clicked = newBoard.find(item => item.x === x && item.y === y);
+    if (clicked && clicked.value === "x") {
+      setGameOver(true);
       return;
+    }
+    if (clicked) {
+      clicked.clicked = true;
 
-    reveal(i, j);
-    if (board[i][j] === "x") setGameOver(true);
-    if (board[i][j] === "o") {
-      // reveal surrounding tiles
-      document.getElementById(`#${i}${j - 1}`)?.click();
-      document.getElementById(`#${i - 1}${j - 1}`)?.click();
-      document.getElementById(`#${i - 1}${j}`)?.click();
-      document.getElementById(`#${i - 1}${j + 1}`)?.click();
-      document.getElementById(`#${i}${j + 1}`)?.click();
-      document.getElementById(`#${i + 1}${j - 1}`)?.click();
-      document.getElementById(`#${i + 1}${j}`)?.click();
-      document.getElementById(`#${i + 1}${j + 1}`)?.click();
+      if (clicked.value === "o") {
+        const neighbors = getNeighbors(x, y);
+        neighbors.forEach(neighbor => {
+          const neighborItem = newBoard.find(
+            item => item.x === neighbor.x && item.y === neighbor.y
+          );
+          if (neighborItem && !neighborItem.clicked) {
+            handleClick(neighbor.x, neighbor.y);
+          }
+        });
+      }
+
+      setBoard(newBoard);
     }
   };
 
-  useEffect(() => {
-    getBoard();
-  }, []);
+  const handleNewGame = () => {
+    axios
+      .get("https://shadify.dev/api/minesweeper/generator?start=1-2")
+      .then(res => {
+        const { data } = res;
+        setBoard(addCoords(data.board));
+        setWidth(data.width);
+        setHeight(data.height);
+        setGameOver(false);
+        setGameWon(false);
+        setTime(0);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(handleNewGame, []);
 
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div className="grid grid-rows-[repeat(12,minmax(0,2rem))] p-2 bg-gray-700 rounded-2xl gap-2 min-w-[24rem]">
-        <div className="row-span-2">
-          <h1>
-            {gameOver ? "Game Over" : gameWon ? "Game Won" : "Minesweeper"}
-          </h1>
-        </div>
-        {board.map((row, i) => {
-          return (
-            <div className="grid grid-cols-9 gap-2">
-              {row.map((col, j) => {
-                return (
-                  <button
-                    key={`#${i}${j}`}
-                    id={`#${i}${j}`}
-                    className={`${
-                      clicked[i][j] ? "bg-red-500" : "bg-blue-700"
-                    } rounded-2xl`}
-                    onClick={() => handleClick(i, j)}
-                  >
-                    {clicked[i][j] && col !== "o" ? col : ""}
-                  </button>
-                );
-              })}
-            </div>
-          );
-        })}
+    <div className="flex flex-col items-center justify-center h-full overflow-y-scroll text-3xl font-bold bg-[#00000011] select-none noscroll p-8 pt-24">
+      <div
+        style={{ width: `calc(4rem * ${width})` }}
+        className="w-full h-[6rem] p-2 text-white"
+      >
+        <h1>Minesweeper</h1>
+      </div>
+      <div
+        style={{
+          width: "fit-content",
+          height: "fit-content",
+          gridTemplateColumns: `repeat(${width}, 1fr)`,
+          gridTemplateRows: `repeat(${height}, 1fr)`,
+          display: "grid",
+        }}
+      >
+        {board.map((item, i) => (
+          <motion.div
+            key={`tile#-${i}`}
+            initial={{ opacity: 0, scale: 0, backgroundColor: "#f2f3f5" }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+              backgroundColor: item.clicked ? "#6b727f" : "#f2f3f5",
+            }}
+            transition={{ delay: item.x * 0.05 + item.y * 0.05 }}
+            className={`flex items-center justify-center w-[4rem] h-[4rem] border border-black `}
+            onClick={() => handleClick(item.x, item.y)}
+          >
+            {item.clicked && item.value !== "o"
+              ? item.value
+              : gameOver && item.value === "x"
+              ? "💣"
+              : ""}
+          </motion.div>
+        ))}
       </div>
     </div>
   );
