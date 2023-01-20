aaded288f0ba2bc1e30856cbc1e182cf074d75f9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-20 07:49:24 +0100

    animate post reveal

--- src/Components/Overlay/Feed/HackerNews/Tab.tsx
+++ src/Components/Overlay/Feed/HackerNews/Tab.tsx
@@ -1,35 +1,24 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import getStories from "../../../../Utils/apis";
 import convertTimestamp from "../../../../Utils/convertTimeStamp";
 import Loading from "../../../Misc/Loading/Loading";
 
 const Tab = ({ currentTab }: { currentTab: string }) => {
-  const [stories, setStories] = React.useState<any[]>([]);
-  const [url, setUrl] = React.useState(
+  const [stories, setStories] = useState<any[]>([]);
+  const [url, setUrl] = useState(
     "https://hacker-news.firebaseio.com/v0/topstories.json"
   );
-  const [isloading, setIsLoading] = React.useState(false);
-  React.useEffect(() => {
-    switch (currentTab) {
-      case "top":
-        setUrl("https://hacker-news.firebaseio.com/v0/topstories.json");
-        break;
-      case "new":
-        setUrl("https://hacker-news.firebaseio.com/v0/newstories.json");
-        break;
-      case "ask":
-        setUrl("https://hacker-news.firebaseio.com/v0/askstories.json");
-        break;
-      case "show":
-        setUrl("https://hacker-news.firebaseio.com/v0/showstories.json");
-        break;
-      case "jobs":
-        setUrl("https://hacker-news.firebaseio.com/v0/jobstories.json");
-        break;
-    }
+  const [isloading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const url = `https://hacker-news.firebaseio.com/v0/${currentTab.replace(
+      "jobs",
+      "job"
+    )}stories.json`;
+    setUrl(url);
   }, [currentTab]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setIsLoading(true);
     const fetchData = async () => {
       const stories = await getStories(url);
@@ -42,14 +31,39 @@ const Tab = ({ currentTab }: { currentTab: string }) => {
   return (
     <div className="flex flex-col h-full gap-2 p-2">
       {isloading ? (
-        <Loading />
+        <div className="pt-24">
+          <Loading />
+        </div>
       ) : (
-        stories.map(story => {
+        stories.map((story, i) => {
           return (
-            <a
+            <motion.a
               href={story.url}
+              initial={{
+                opacity: 0,
+                backgroundColor: "#1a202c",
+                backdropFilter: "blur(10px)",
+                borderRadius: "1rem",
+              }}
+              animate={{ opacity: 1 }}
+              whileHover={{
+                backgroundColor: "#1a202c",
+                backgroundImage:
+                  "linear-gradient(90deg, rgba(255,255,255,0.1) 0%, rgba(255,255,255,0.1) 50%, rgba(255,255,255,0) 100%)",
+                borderRadius: "2rem",
+              }}
+              transition={{
+                // @ts-ignore
+                type: "spring",
+                // @ts-ignore
+                damping: 10,
+                // @ts-ignore
+                stiffness: 100,
+                opacity: { delay: i * 0.05 },
+              }}
               key={story.id}
-              className="flex flex-col p-2 transition-all duration-[400ms] rounded hover:backdrop-blur-xl active:scale-95 hover:ring-1 ring-current hover:shadow-xl"
+              className="flex flex-col p-4"
+              target={"_blank"}
             >
               <span className="w-full overflow-hidden font-medium text-ellipsis whitespace-nowrap">
                 {story.title}
@@ -59,7 +73,7 @@ const Tab = ({ currentTab }: { currentTab: string }) => {
               <span className="text-gray-300">
                 Time: {convertTimestamp(story.time)}
               </span>
-            </a>
+            </motion.a>
           );
         })
       )}
