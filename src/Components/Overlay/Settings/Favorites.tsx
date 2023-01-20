64aa09c5c9bb3522d856327c0849471924da87b2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-20 11:46:47 +0100

    support small screens

--- src/Components/Overlay/Settings/Favorites.tsx
+++ src/Components/Overlay/Settings/Favorites.tsx
@@ -16,9 +16,9 @@ const Favorites = ({
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="h-[78%] w-[90%] absolute top-8 left-1/2 -translate-x-1/2 grid grid-cols-4 grid-rows-[1.25rem,repeat(3,minmax(0,1fr))] gap-8 "
+      className="h-[78%] w-[90%] absolute top-8 left-1/2 -translate-x-1/2 grid lg:grid-cols-4 grid-cols-3 grid-rows-[1.25rem,repeat(4,minmax(0,1fr))] lg:grid-rows-[1.25rem,repeat(3,minmax(0,1fr))] gap-8 transition-all"
     >
-      <h1 className="row-span-1 col-span-4 text-center text-3xl font-semibold">
+      <h1 className="col-span-3 row-span-1 text-3xl font-semibold text-center sm:col-span-4">
         Top Sites
       </h1>
       {favId === null ? (
@@ -45,7 +45,7 @@ const Favorites = ({
       ) : (
         <form
           className="absolute flex flex-col items-center gap-6 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-          onSubmit={(e) => {
+          onSubmit={e => {
             e.preventDefault();
             const newFavorites = favorites;
             newFavorites[favId] = {
@@ -63,7 +63,7 @@ const Favorites = ({
               type="url"
               id="url"
               value={url}
-              onChange={(e) => setUrl(e.target.value)}
+              onChange={e => setUrl(e.target.value)}
               required
               className="h-12 font-semibold text-center text-black transition-all rounded-lg shadow-lg w-80 hover:bg-slate-200"
             />
@@ -74,7 +74,7 @@ const Favorites = ({
               type="url"
               id="icon"
               value={iconLink}
-              onChange={(e) => setIconLink(e.target.value)}
+              onChange={e => setIconLink(e.target.value)}
               className="h-12 font-semibold text-center text-black transition-all rounded-lg shadow-lg w-80 hover:bg-slate-200"
             />
           </div>
