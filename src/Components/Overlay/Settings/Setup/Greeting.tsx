e9d4e301c88e691b2419182495b2e17516241f61
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-20 11:36:12 +0100

    center text

--- src/Components/Overlay/Settings/Setup/Greeting.tsx
+++ src/Components/Overlay/Settings/Setup/Greeting.tsx
@@ -3,13 +3,11 @@ import { motion } from "framer-motion";
 const Greeting = () => {
   return (
     <motion.div
-      className="flex items-center justify-center flex-col text-5xl font-[GreatVibes] tracking-wide font-light gap-8"
+      className="flex items-center justify-center flex-col text-5xl font-[GreatVibes] font-bold tracking-wide gap-8 text-center"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <h1 className=" ">
-        Hello, Thank you for using this extension, I hope you'll love it.
-      </h1>
+      <h1>Hello, Thank you for using this extension, I hope you'll love it.</h1>
       <p>But first, let's set up a few things..</p>
     </motion.div>
   );
