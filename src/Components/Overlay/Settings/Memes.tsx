9ba5ebc18e55f903c30150668f44fdcf25b0f070
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-20 10:26:00 +0100

    tweak look

--- src/Components/Overlay/Settings/Memes.tsx
+++ src/Components/Overlay/Settings/Memes.tsx
@@ -29,50 +29,73 @@ const Memes = ({ memes, setMemes }: { memes: any; setMemes: any }) => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="flex flex-col gap-4"
+      className="grid grid-cols-2 gap-4 pt-8 font-bold"
     >
       <div
         onClick={() => toggleNSFW()}
-        className={`rounded text-xl p-2 px-8 text-center transition-all cursor-pointer flex w-full justify-between items-center gap-16  select-none `}
+        className="flex items-center justify-between w-full col-span-2 gap-16 p-2 px-8 text-xl text-center transition-all rounded cursor-pointer select-none"
       >
         NSFW
         <div
-          className={` transition-all w-12 rounded-full relative h-4  ${
-            memes.isNsfwEnabled ? "bg-green-500" : "bg-red-500"
-          } bg-opacity-25 `}
+          className={
+            "transition-all w-12 rounded-full relative h-4 bg-opacity-50 " +
+            (memes.isNsfwEnabled ? "bg-green-500" : "bg-red-500")
+          }
         >
-          <span
-            className={`rounded-full transition-all ${
+          <motion.span
+            initial={{
+              top: -4,
+              left: memes.isNsfwEnabled ? "80%" : "0",
+              padding: 0,
+            }}
+            animate={{
+              left: memes.isNsfwEnabled ? 24 : 0,
+              padding: 3,
+            }}
+            transition={{ type: "spring", damping: 10, stiffness: 100 }}
+            className={`rounded-full ${
               memes.isNsfwEnabled
-                ? "bg-green-500 text-green-800 left-[80%]"
-                : "bg-red-500 text-red-800 left-2"
-            } absolute top-1/2 -translate-y-1/2 -translate-x-1/2 text-base p-1`}
+                ? "bg-green-500 text-green-800"
+                : "bg-red-500 text-red-800"
+            } absolute`}
           >
             {memes.isNsfwEnabled ? <FiCheck /> : <FiX />}
-          </span>
+          </motion.span>
         </div>
       </div>
       {sources.map((meme: any, i: number) => (
-        <div
+        <motion.button
           key={i + meme.name}
+          initial={{
+            opacity: 0,
+            padding: ".5rem",
+            backgroundColor: meme.isEnabled ? "#22c55e" : "#ef4444",
+            borderRadius: "1rem",
+            scale: 0.95,
+          }}
+          animate={{
+            opacity: 1,
+            padding: ".75rem",
+            backgroundColor: meme.isEnabled ? "#22c55e" : "#ef4444",
+            borderRadius: "1rem",
+            scale: 1,
+          }}
+          whileHover={{ borderRadius: "1.5rem" }}
+          whileTap={{ scale: 0.95, borderRadius: "1.5rem" }}
+          transition={{ type: "spring", damping: 10, stiffness: 100 }}
           onClick={() => toggleSource(i)}
-          className={`rounded text-xl p-2 px-8 text-center transition-all cursor-pointer flex w-full justify-between items-center gap-12 ${
-            meme.isEnabled
-              ? "bg-green-500 text-green-500"
-              : "bg-red-500 text-red-500"
-          } select-none active:scale-95 bg-opacity-10 hover:bg-opacity-25 backdrop-blur-xl`}
         >
-          r/{meme.name} {meme.isEnabled ? <FiCheck /> : <FiX />}
-        </div>
+          r/{meme.name}
+        </motion.button>
       ))}
       {/* // TODO: Will need to test this first and make sure users don't break it using links or wrong subreddits */}
       {/* <input
-          type="text"
-          placeholder="Custom Subreddit"
-          className="text-center text-blue-500 bg-blue-500 border-none rounded outline-none bg-opacity-10 hover:bg-opacity-25 placeholder:text-blue-500"
-        />
+            type="text"
+            placeholder="Custom Subreddit"
+            className="text-center text-blue-500 bg-blue-500 border-none rounded outline-none bg-opacity-10 hover:bg-opacity-25 placeholder:text-blue-500"
+          />
 
-        */}
+          */}
     </motion.div>
   );
 };
