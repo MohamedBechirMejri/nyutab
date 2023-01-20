48af2f7a8972d04cf72681e53ed975dd18472524
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-20 11:56:30 +0100

    support small screens

--- src/Components/Overlay/Games/Games.tsx
+++ src/Components/Overlay/Games/Games.tsx
@@ -38,7 +38,7 @@ const buttons = [
   {
     name: "2048",
     soon: true,
-    className: "col-span-2",
+    className: "sm:col-span-2",
     color: "#eab308",
     overlay: "games",
   },
@@ -47,7 +47,7 @@ const buttons = [
 const Games = ({ setOverlay }: { setOverlay: any }) => {
   return (
     <div className="flex flex-col items-center h-full overflow-y-scroll text-3xl font-bold bg-[#00000011] select-none noscroll p-8 pt-24">
-      <div className="grid w-full h-full grid-cols-3 gap-4 p-4">
+      <div className="grid w-full h-full max-w-5xl gap-4 p-4 sm:grid-cols-3">
         {buttons.map((button, i) => (
           <Button button={button} i={i} setOverlay={setOverlay} />
         ))}
