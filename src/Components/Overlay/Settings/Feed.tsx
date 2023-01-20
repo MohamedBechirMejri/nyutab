c181b200b5d516778b8e9bea48f58a662eedabd8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-20 10:54:10 +0100

    tweak input

--- src/Components/Overlay/Settings/Feed.tsx
+++ src/Components/Overlay/Settings/Feed.tsx
@@ -59,32 +59,6 @@ const Memes = ({ feed, setFeed }: { feed: any; setFeed: any }) => {
       className="grid grid-cols-2 gap-4"
     >
       {feed.rss.sources.map((source: any, i: number) => (
-        // <div
-        //   key={i + source.name}
-        //   className={`rounded text-xl p-2 px-8 text-center transition-all cursor-pointer flex w-full justify-between items-center gap-12 bg-opacity-20 capitalize ${
-        //     source.isEnabled
-        //       ? "bg-green-500 text-green-500"
-        //       : "bg-red-500 text-red-500"
-        //   }`}
-        //   onClick={() => toggleSource(i)}
-        // >
-        //   {source.name}
-        //   <div
-        //     className={` transition-all w-12 rounded-full relative h-4  ${
-        //       source.isEnabled ? "bg-green-500" : "bg-red-500"
-        //     } bg-opacity-25 `}
-        //   >
-        //     <span
-        //       className={`rounded-full transition-all ${
-        //         source.isEnabled
-        //           ? "bg-green-500 text-green-800 left-[80%]"
-        //           : "bg-red-500 text-red-800 left-2"
-        //       } absolute top-1/2 -translate-y-1/2 -translate-x-1/2 text-base p-1`}
-        //     >
-        //       {source.isEnabled ? <FiCheck /> : <FiX />}
-        //     </span>
-        //   </div>
-        // </div>
         <motion.button
           key={i + source.name}
           initial={{
@@ -110,20 +84,20 @@ const Memes = ({ feed, setFeed }: { feed: any; setFeed: any }) => {
         </motion.button>
       ))}
 
-      <div className="flex gap-4 font-bold text-black">
+      <div className="flex col-span-2 gap-4 font-bold">
         <input
           type="text"
           placeholder="name"
           value={name}
           onChange={e => setName(e.target.value)}
-          className=""
+          className="bg-transparent"
         />
         <input
           type="url"
           placeholder="url"
           value={url}
           onChange={e => setUrl(e.target.value)}
-          className=""
+          className="bg-transparent"
         />
         <button className="text-white" onClick={() => addSource()}>
           <FiPlus />
