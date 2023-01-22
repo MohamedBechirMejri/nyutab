8e36fadcdb497d6d3ac76d1ce070f0212c6cd250
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-22 19:55:50 +0100

    remove everything

--- src/Components/Overlay/Games/2048/index.tsx
+++ src/Components/Overlay/Games/2048/index.tsx
@@ -2,61 +2,6 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 const X2048 = () => {
-  const boardWidth = 4;
-  const boardHeight = 4;
-
-  return (
-    <div className="flex flex-col items-center justify-center h-full gap-4 pt-24 bg-slate-900 bg-opacity-70">
-      <motion.div
-        className="flex flex-col items-center justify-center w-full h-full gap-4"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ ease: "anticipate", duration: 0.3 }}
-      >
-        <h1 className="text-4xl font-bold">2048</h1>
-        <div className="flex items-center justify-center w-full gap-4 h-max">
-          <div className="flex items-center justify-center w-full h-12 text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl">
-            Score: {0}
-          </div>
-          <div className="flex items-center justify-center w-full h-12 text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl">
-            Best: {0}
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center w-full h-12 text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl">
-          <button className="w-full h-full text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl">
-            Start
-          </button>
-        </div>
-
-        <div className="flex items-center justify-center w-full h-12 text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl">
-          <button
-            className="w-full h-full text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl"
-            onClick={() => console.log("left")}
-          >
-            Left
-          </button>
-          <button
-            className="w-full h-full text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl"
-            onClick={() => console.log("right")}
-          >
-            Right
-          </button>
-          <button
-            className="w-full h-full text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl"
-            onClick={() => console.log("up")}
-          >
-            Up
-          </button>
-          <button
-            className="w-full h-full text-2xl font-bold text-slate-100 bg-slate-800 rounded-xl"
-            onClick={() => console.log("down")}
-          >
-            Down
-          </button>
-        </div>
-      </motion.div>
-    </div>
-  );
+  return <div></div>;
 };
 export default X2048;
