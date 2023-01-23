1fe4a14d59cd2c9e10eadbd7df83e154b8b7f2f1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-23 01:35:52 +0100

    remove unused code

--- src/Components/Overlay/Games/2048/index.tsx
+++ src/Components/Overlay/Games/2048/index.tsx
@@ -2,47 +2,6 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { getRandomNumber } from "../../../../lib/mathUtils";
 
-/**
- * @description 2048 game
- * @returns {JSX.Element} 2048 game
- * @constructor
- * @see
- *
- * @todo
- * - [ ] Add swipe controls
- * - [ ] Add keyboard controls
- * - [ ] Add game over screen
- * - [ ] Add win screen
- * - [ ] Add game restart
- * - [ ] Add game settings
- * - [ ] Add game save
- * - [ ] Add game load
- * - [ ] Add game undo
- * - [ ] Add game redo
- * - [ ] Add game reset
- * - [ ] Add game pause
- * - [ ] Add game resume
- * - [ ] Add game quit
- *
- * @logic
- * - the game board is a 4x4 grid
- * - the game board is an array of 16 cells
- * - each cell is an object with a value and a position { value: number, x: number, y: number}
- * - the game board is initialized with 2 cells with random values and random positions
- * - the game board is updated by swiping in a direction
- * - the cells are placed in the ui by their positions in motion.div from framer motion
- * - the cells are updated by their values in motion.div from framer motion
- * - the cells are updated by their positions in motion.div from framer motion
- * -
- * - the cells are stored in a 1D array
- * - the cells positions are updated then we filter the new array to remove the empty cells or duplicate cells that are in same position
- * - the positions are are updated by the direction of the swipe and the amount of the empty cells in the direction of the swipe ie: if the swipe is left and there are 2 empty cells to the left of the cell then the position of the cell is updated by 2 but if there is another cell to the left of the cell then the position of the cell is the same unless the value of the cell is the same as the value of the cell to the left of it then the position of the cell is updated by 1
- * after the positions are updated we filter the array to remove the empty cells and duplicate cells that are in the same position by keeping one of them and doubling its value and resetting the other one
- * we then check for empty cells and if there are any we generate a new cell with a random value and a random position
- * if not then the game is over
- * new cells value is either 2 or 4 and the probability of getting a 4 is 10%
- */
-
 const X2048 = () => {
   const boardWidth = 4;
   const boardHeight = 4;
@@ -58,27 +17,7 @@ const X2048 = () => {
 
   const moveUp = () => {
     setBoard(board => {
-      let newBoard = board;
-
-      for (let i = 0; i < newBoard.length; i++) {
-        const cell = newBoard[i];
-        if (cell.y === 0 || cell.y === null) continue;
-
-        const cellsAbove = newBoard.filter(c => c.y < cell.y && c.x === cell.x);
-        const directCellAbove = cellsAbove[cellsAbove.length - 1];
-        let distance;
-
-        if (
-          directCellAbove &&
-          directCellAbove.value &&
-          directCellAbove.value !== cell.value
-        )
-          distance = cell.y - directCellAbove.y - 1;
-        else distance = cell.y;
-
-        cell.y -= distance;
-        newBoard = [...mergeTiles(newBoard)];
-      }
+      let newBoard = [...board];
 
       return [...newBoard];
     });
@@ -86,27 +25,7 @@ const X2048 = () => {
 
   const moveDown = () => {
     setBoard(board => {
-      let newBoard = board;
-
-      for (let i = 0; i < newBoard.length; i++) {
-        const cell = newBoard[i];
-        if (cell.y === boardHeight - 1 || cell.y === null) continue;
-
-        const cellsBelow = newBoard.filter(c => c.y > cell.y && c.x === cell.x);
-        const directCellBelow = cellsBelow[0];
-        let distance;
-
-        if (
-          directCellBelow &&
-          directCellBelow.value &&
-          directCellBelow.value !== cell.value
-        )
-          distance = directCellBelow.y - cell.y - 1;
-        else distance = boardHeight - cell.y - 1;
-
-        cell.y += distance;
-        newBoard = [...mergeTiles(newBoard)];
-      }
+      let newBoard = [...board];
 
       return [...newBoard];
     });
@@ -114,27 +33,7 @@ const X2048 = () => {
 
   const moveLeft = () => {
     setBoard(board => {
-      let newBoard = board;
-
-      for (let i = 0; i < newBoard.length; i++) {
-        const cell = newBoard[i];
-        if (cell.x === 0 || cell.x === null) continue;
-
-        const cellsLeft = newBoard.filter(c => c.x < cell.x && c.y === cell.y);
-        const directCellLeft = cellsLeft[cellsLeft.length - 1];
-        let distance;
-
-        if (
-          directCellLeft &&
-          directCellLeft.value &&
-          directCellLeft.value !== cell.value
-        )
-          distance = cell.x - directCellLeft.x - 1;
-        else distance = cell.x;
-
-        cell.x -= distance;
-        newBoard = [...mergeTiles(newBoard)];
-      }
+      let newBoard = [...board];
 
       return [...newBoard];
     });
@@ -142,27 +41,7 @@ const X2048 = () => {
 
   const moveRight = () => {
     setBoard(board => {
-      let newBoard = board;
-
-      for (let i = 0; i < newBoard.length; i++) {
-        const cell = newBoard[i];
-        if (cell.x === boardWidth - 1 || cell.x === null) continue;
-
-        const cellsRight = newBoard.filter(c => c.x > cell.x && c.y === cell.y);
-        const directCellRight = cellsRight[0];
-        let distance;
-
-        if (
-          directCellRight &&
-          directCellRight.value &&
-          directCellRight.value !== cell.value
-        )
-          distance = directCellRight.x - cell.x - 1;
-        else distance = boardWidth - cell.x - 1;
-
-        cell.x += distance;
-        newBoard = [...mergeTiles(newBoard)];
-      }
+      let newBoard = [...board];
 
       return [...newBoard];
     });
