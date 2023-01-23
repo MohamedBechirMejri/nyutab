ac00a2fbd280eb15d89c231a7c82e99e0d5594f9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-23 01:04:43 +0100

    clarify function param

--- src/lib/mathUtils.ts
+++ src/lib/mathUtils.ts
@@ -1,3 +1,3 @@
-export const getRandomNumber = (max: number, min: number = 0) => {
-  return Math.floor(Math.random() * max + min);
+export const getRandomNumber = (lessThan: number, min: number = 0) => {
+  return Math.floor(Math.random() * lessThan + min);
 };
