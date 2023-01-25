759af6647b71018744b4a3e30d3260715f2af3cb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-25 12:57:07 +0100

    tweak positions

--- src/Components/Overlay/Games/Wordle/index.tsx
+++ src/Components/Overlay/Games/Wordle/index.tsx
@@ -22,19 +22,19 @@ const Wordle = () => {
   const [board, setBoard] = useState(generateBoard(word));
 
   return (
-    <div className="flex flex-col items-center justify-center h-full pt-24 bg-whjite bg-opacity-10">
-      <motion.div
-        initial={{ opacity: 0, y: 100, scale: 0.5 }}
-        animate={{ opacity: 1, y: 0, scale: 1 }}
-      >
-        <h1>Wordle</h1>
-        <div className="grid grid-rows-6 w-[min(34rem,90vw)] bg-[#0fa5e9] border border-sky-200">
-          {board.map((row, i) => (
-            <Row key={"row" + i} rowIndex={i} word={word} row={row} />
-          ))}
-        </div>
-      </motion.div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 100, scale: 0.5 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      className="h-full grid grid-rows-[6rem,1fr,2fr,1fr] items-center justify-center"
+    >
+      <h1 className="pt-4 text-2xl font-bold text-center">Wordle</h1>
+      <div></div>
+      <div className="grid grid-rows-6 w-[min(34rem,90vw)] bg-[#0fa5e9] border border-sky-200">
+        {board.map((row, i) => (
+          <Row key={"row" + i} rowIndex={i} word={word} row={row} />
+        ))}
+      </div>
+    </motion.div>
   );
 };
 
