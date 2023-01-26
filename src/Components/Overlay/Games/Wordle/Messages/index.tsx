8bc79d1774768564ad74b4b365857c65741d293f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-26 14:01:54 +0100

    show game won message

--- src/Components/Overlay/Games/Wordle/Messages/index.tsx
+++ src/Components/Overlay/Games/Wordle/Messages/index.tsx
@@ -1,8 +1,27 @@
-const Messages = () => {
+const Messages = ({
+  word,
+  isGameOver,
+  isGameWon,
+  message,
+  score,
+  setMessage,
+}: {
+  word: string;
+  isGameOver: boolean;
+  isGameWon: boolean;
+  message: string;
+  score: number;
+  setMessage: (message: string) => void;
+}) => {
   return (
     <div className="grid h-full grid-cols-2 grid-rows-1">
       <div className="flex items-center justify-center h-full text-xl font-bold bg-zinc-800">
-        <h1>Word: {"word"}</h1>
+        <h1>
+          Word: {word}
+          {isGameOver && isGameWon && (
+            <span className="text-green-500"> - Correct!</span>
+          )}
+        </h1>
       </div>
       <div className="grid h-full grid-cols-2 grid-rows-1">
         <button>assistant placeholder</button>
