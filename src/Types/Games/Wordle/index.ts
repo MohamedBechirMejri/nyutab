9eea0be629e2ba9c7a26ffc0af5a266b27fbd90e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-26 11:53:53 +0100

    update letter type

--- src/Types/Games/Wordle/index.ts
+++ src/Types/Games/Wordle/index.ts
@@ -1,5 +1,5 @@
 export type $Letter = {
   id: string;
   letter: string | null;
-  status: "empty" | "filled" | "correct" | "incorrect" | "misplaced";
+  status: "empty" | "correct" | "incorrect" | "misplaced";
 };
