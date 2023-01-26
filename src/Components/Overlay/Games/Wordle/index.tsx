a4c9548763915d4b20e12ac3bb333868438b8b40
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-26 13:26:26 +0100

    add placeholders

--- src/Components/Overlay/Games/Wordle/index.tsx
+++ src/Components/Overlay/Games/Wordle/index.tsx
@@ -99,7 +99,15 @@ const Wordle = () => {
       className="h-full grid grid-rows-[6rem,.5fr,3fr,1.5fr] grid-cols-1 gap-2 items-center justify-center select-none w-[min(34rem,90vw)] mx-auto"
     >
       <h1 className="pt-4 text-2xl font-bold text-center">Wordle</h1>
-      <div></div>
+      <div className="grid h-full grid-cols-2 grid-rows-1">
+        <div className="flex items-center justify-center h-full text-xl font-bold bg-zinc-800">
+          <h1>Word: {word}</h1>
+        </div>
+        <div className="grid h-full grid-cols-2 grid-rows-1">
+          <button>assistant placeholder</button>
+          <button>restart</button>
+        </div>
+      </div>
       <div className="grid grid-rows-6 bg-[#0fa5e9] border border-sky-200 h-full">
         {board.map((row: $Letter[], i) => (
           <Row key={"row" + i} word={word} row={row} />
