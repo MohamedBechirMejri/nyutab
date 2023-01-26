42f738b0f7bca819c26c2601a4c3a7b5f358d633
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-26 12:46:01 +0100

    colorize based on status

--- src/Components/Overlay/Games/Wordle/Row/Letter.tsx
+++ src/Components/Overlay/Games/Wordle/Row/Letter.tsx
@@ -2,15 +2,39 @@ import type { $Letter } from "../../../../../Types/Games/Wordle";
 
 import { motion } from "framer-motion";
 
-
 const Letter = ({ letter }: { letter: $Letter }) => {
   return (
     <div className="h-[min(5.5rem,100%)] border border-sky-200 bg-gradient-to-b from-sky-400">
-      <p className="flex items-center justify-center w-full h-full text-xl font-bold">
+      <p
+        className="flex items-center justify-center w-full h-full text-xl font-bold uppercase"
+        style={{
+          padding: letter.status === "misplaced" ? "0.25rem" : "0",
+        }}
+      >
         {letter.letter ? (
           <motion.span
-            initial={{ opacity: 0, y: 100, scale: 0.5 }}
-            animate={{ opacity: 1, y: 0, scale: 1 }}
+            initial={{
+              opacity: 0,
+              y: 100,
+              scale: 0.5,
+              backgroundColor: "ffffff00",
+              borderRadius: "0",
+              color: "white",
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              scale: 1,
+              backgroundColor:
+                letter.status === "correct" ? "#f00" : "#ffffff00",
+              backgroundImage:
+                letter.status === "misplaced"
+                  ? ["linear-gradient(45deg,gold,#ff0)"]
+                  : "none",
+              borderRadius: letter.status === "misplaced" ? "99999rem" : "0rem",
+              color: letter.status === "misplaced" ? "orange" : "white",
+            }}
+            className="flex items-center justify-center w-full h-full"
           >
             {letter.letter}
           </motion.span>
