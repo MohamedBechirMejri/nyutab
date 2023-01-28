1eb8c94008e9e4b35144ba14a7fcee955394a0a8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-28 15:55:00 +0100

    add mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import mdx from "@mdx-js/rollup";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    { enforce: "pre", ...mdx(/* jsxImportSource: …, otherOptions… */) },
+    react(),
+  ],
   build: { outDir: "build" },
   server: { port: 3000 },
 });
