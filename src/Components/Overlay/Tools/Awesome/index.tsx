74a18a095ca673b230cf15bf41fae89a2204c64b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-28 15:20:21 +0100

    delete old code

--- src/Components/Overlay/Tools/Awesome/index.tsx
+++ src/Components/Overlay/Tools/Awesome/index.tsx
@@ -1,98 +1,19 @@
-import reposList from "../../../../db/awesome/repos.json";
-import { decompress } from "compress-json";
-import { useEffect, useState } from "react";
-import { getRandomNumber } from "../../../../lib/mathUtils";
-
-// TODO: Filter Broken links, Improve Look, Fix Slow Performance, Add random site button
-
 const Awesome = () => {
-  const [categories, setCategories] = useState<any>(null);
-  const [category, setCategory] = useState<any>(null);
-  const [repos, setRepos] = useState<any>([]);
-
-  useEffect(() => {
-    // @ts-ignore
-    const data = decompress(reposList);
-    const categories = Object.entries(data);
-    console.log(categories);
-    setCategories(categories.sort());
-    setCategory(categories[getRandomNumber(categories.length)]);
-  }, []);
-
-  useEffect(() => {
-    if (!category) return;
-    (async () => {
-      const repos = await category[1].map(async (repository: any) => {
-        const { name, repo, url } = repository;
-        const links = await import(
-          `../../../db/awesome/repos/${repo.replace("/", "-")}.json`
-        );
-
-        setRepos((repos: any) => [
-          ...repos,
-          {
-            name,
-            url,
-            links,
-          },
-        ]);
-      });
-    })();
-  }, [category]);
-
-  useEffect(() => {
-    console.log(repos);
-  }, [repos]);
-
-  return categories ? (
-    <div className="grid w-full h-full grid-cols-6 grid-rows-1 p-8 rounded-lg bg-slate-800">
-      <div className="flex flex-col h-full gap-4 p-4 overflow-scroll noscroll">
-        {categories.map(([name]: [name: string], i: number) => {
-          return (
-            <button
-              key={name}
-              onClick={() => {
-                setRepos([]);
-                setCategory(categories[i]);
-              }}
-            >
-              {name}
-            </button>
-          );
-        })}
-      </div>
-      {/* {category && ( */}
-      <div className="flex flex-col w-full h-full col-span-5 gap-2 p-8 overflow-scroll rounded-lg noscroll">
-        <h1 className="text-4xl font-semibold">{category[0]}</h1>
-        <div>
-          {repos.map((list: any) => {
-            return (
-              <div>
-                <h2 className="m-4 text-3xl">- {list.name}</h2>
-                <div className="pl-8">
-                  {list.links.default.map((link: any) => (
-                    <div className="flex w-full gap-2 my-2 text-xl">
-                      <span>•</span>
-                      <a
-                        href={link.url}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="text-blue-400 shrink-0"
-                      >
-                        {link.name}
-                      </a>
-                      {link.description && <p>- {link.description}</p>}.
-                    </div>
-                  ))}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      {/* )} */}
+  return (
+    <div className="grid w-full h-full grid-cols-6 grid-rows-[7rem,1fr] font-bold bg-orange-200 rounded-lg text-zinc-900">
+      <h1 className="flex flex-col items-center justify-center col-span-6 text-3xl">
+        Awesome Nyutab
+        <a
+          href="https://github.com/MohamedBechirMejri/nyutab/issues"
+          target="_blank"
+          rel="noreferrer"
+          className="text-sm hover:underline"
+        >
+          Add your own Links
+        </a>
+      </h1>
     </div>
-  ) : null;
+  );
 };
 
 export default Awesome;
