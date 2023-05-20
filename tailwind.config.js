1b769b98995275c4eb608d464b5e71cd7c6cc76f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-05-20 04:37:00 +0100

    remove tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,5 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        borderFade: "borderFade .7s alternate infinite",
-        fadeIn: "fadeIn .5s forwards",
-      },
-      keyframes: {
-        borderFade: {
-          "0%": {
-            borderColor: "currentColor",
-          },
-          "100%": {
-            borderColor: "transparent",
-          },
-        },
-        fadeIn: {
-          "0%": {
-            opacity: "0",
-          },
-          "100%": {
-            opacity: "1",
-          },
-        },
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
+  theme: {},
+  plugins: [],
 };
