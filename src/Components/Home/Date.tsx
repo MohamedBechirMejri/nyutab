907de17f4ce29220eaa24fa52f6286202225a913
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-14 19:18:31 +0100

    Refactor date display in Home component

--- src/Components/Home/Date.tsx
+++ src/Components/Home/Date.tsx
@@ -10,13 +10,10 @@ const Date = () => {
     }, 1000);
     return () => clearInterval(interval);
   }, []);
+
   return (
     <h1 className="flex items-start justify-center 2xl:text-2xl font-bold text-center bg-transparent rounded-lg font-[FiraCode] flex-col">
-      <p className="flex flex-col">
-        <span>{currentDate[0].slice(0, 3)} </span>
-        <span className="pr-4">{currentDate[1].split(" ")[1]}</span>
-        <span>{currentDate[1].split(" ")[0].slice(0, 3)} </span>
-      </p>{" "}
+      {currentDate[0]}, {currentDate[1]}
     </h1>
   );
 };
