b7878f3995939422c63b611dd2dd7046f9583576
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-28 03:59:23 +0100

    Update Home component layout

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,7 +6,7 @@ import Memes from "./Memes";
 const Home = ({ setOverlay }: { setOverlay: any }) => {
   return (
     <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
-      <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8">
+      <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8 grid-rows-1">
         <div className="bg-black"></div>
         <div className="h-full grid grid-rows-[auto,minmax(0,1fr)] place-items-center py-4">
           <div className="p-4 flex flex-col items-center w-max bg-black bg-opacity-30 backdrop-blur-3xl rounded-lg gap-2 shadow">
