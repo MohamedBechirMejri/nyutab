39daebb22cec338f2f693dd99382a84f98a486be
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-28 03:14:07 +0100

    Refactor Home component layout

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,36 +1,31 @@
-import { lazy, Suspense } from "react";
+import Clock from "./Clock/Clock";
+import FactsAndQuotes from "./FactsAndQuotes";
+import FavoriteSites from "./FavoriteSites";
+import Memes from "./Memes";
 
-const Date = lazy(() => import("./Date"));
-const Clock = lazy(() => import("./Clock/Clock"));
-const FavoriteSites = lazy(() => import("./FavoriteSites"));
-const Memes = lazy(() => import("./Memes"));
-const FactsAndQuotes = lazy(() => import("./FactsAndQuotes"));
+
 
 const Home = ({ setOverlay }: { setOverlay: any }) => {
   return (
-    <Suspense>
-      {
-        <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
-          <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8">
-            <div className="bg-black"></div>
-            <div className="h-1/2 m-auto">
-              <Memes setOverlay={setOverlay} />
-            </div>
-            <div className="flex flex-col items-end">
-              <div className="flex">
-                <div className="p-4 flex flex-col items-center w-max bg -black backdrop-blur">
-                  <Clock />
-                  <Date />
-                </div>
-              </div>
-              <FavoriteSites />
+    <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
+      <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8">
+        <div className="bg-black"></div>
+        <div className="h-full flex flex-col justify-center items-center">
+          <div className="flex">
+            <div className="p-4 flex flex-col items-center w-max bg -black backdrop-blur">
+              <Clock />
+              <Date />
             </div>
           </div>
-
-          <FactsAndQuotes />
+          <Memes setOverlay={setOverlay} />
         </div>
-      }
-    </Suspense>
+        <div className="flex flex-col items-end">
+          <FavoriteSites />
+        </div>
+      </div>
+
+      <FactsAndQuotes />
+    </div>
   );
 };
 
