8ee89199b1343328679422a1367360370a0bb1da
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-28 03:10:03 +0100

    Remove unused code and comments

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,50 +1,18 @@
 import { lazy, Suspense } from "react";
-import { motion } from "framer-motion";
-import Feed from "../Overlay/Feed";
 
 const Date = lazy(() => import("./Date"));
 const Clock = lazy(() => import("./Clock/Clock"));
 const FavoriteSites = lazy(() => import("./FavoriteSites"));
 const Memes = lazy(() => import("./Memes"));
 const FactsAndQuotes = lazy(() => import("./FactsAndQuotes"));
-const Button = lazy(() => import("../Misc/Button"));
 
 const Home = ({ setOverlay }: { setOverlay: any }) => {
   return (
     <Suspense>
       {
         <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
-          {/*
-
-          <div className="h-full grid grid-cols-3 place-items-center">
-
-
-            <div className="grid grid-cols-4 row-span-2 gap-4 p-4">
-              <Button
-                name={"Games"}
-                handleClick={() => setOverlay("games")}
-                className="text-red-500 bg-red-500 bg-opacity-10 hover:bg-opacity-20"
-              />
-              <Button
-                name={"Tools"}
-                handleClick={() => setOverlay("tools")}
-                className="text-yellow-500 bg-yellow-500 bg-opacity-10 hover:bg-opacity-20"
-              />
-              <Button
-                name={"Feed"}
-                handleClick={() => setOverlay("feed")}
-                className="text-blue-500 bg-blue-500 bg-opacity-10 hover:bg-opacity-20"
-              />
-              <Button
-                name={"Settings"}
-                handleClick={() => setOverlay("settings")}
-                className="text-pink-500 bg-pink-500 bg-opacity-10 hover:bg-opacity-20"
-              />
-            </div>
-          </div>*/}
           <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8">
             <div className="bg-black"></div>
-            {/* <Feed /> */}
             <div className="h-1/2 m-auto">
               <Memes setOverlay={setOverlay} />
             </div>
