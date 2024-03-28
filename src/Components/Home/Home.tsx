11ea0952874e38f736dc27bca0e4d87d4abba8a1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-28 03:34:24 +0100

    Refactor Home component layout

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,19 +3,15 @@ import FactsAndQuotes from "./FactsAndQuotes";
 import FavoriteSites from "./FavoriteSites";
 import Memes from "./Memes";
 
-
-
 const Home = ({ setOverlay }: { setOverlay: any }) => {
   return (
     <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
       <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8">
         <div className="bg-black"></div>
-        <div className="h-full flex flex-col justify-center items-center">
-          <div className="flex">
-            <div className="p-4 flex flex-col items-center w-max bg -black backdrop-blur">
-              <Clock />
-              <Date />
-            </div>
+        <div className="h-full grid grid-rows-[auto,minmax(0,1fr)] place-items-center py-4">
+          <div className="p-4 flex flex-col items-center w-max bg-black bg-opacity-30 backdrop-blur-3xl rounded-lg gap-2 shadow">
+            <Clock />
+            <Date />
           </div>
           <Memes setOverlay={setOverlay} />
         </div>
