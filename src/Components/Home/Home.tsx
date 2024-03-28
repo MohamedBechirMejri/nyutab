1ec7972dd40a47dbbd14266fb056bd338f92ff77
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-28 04:32:20 +0100

    Add miniApp state and useMemo hook to Home component

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,18 @@
+import { useMemo, useState } from "react";
 import Clock from "./Clock/Clock";
 import FactsAndQuotes from "./FactsAndQuotes";
 import FavoriteSites from "./FavoriteSites";
 import Memes from "./Memes";
 
 const Home = ({ setOverlay }: { setOverlay: any }) => {
+  const [miniApp, setMiniApp] = useState("memes");
+
+  const miniApps = useMemo(() => {
+    return {
+      memes: <Memes setOverlay={setOverlay} />,
+    };
+  }, []) as any;
+
   return (
     <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
       <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8 grid-rows-1">
@@ -13,7 +22,7 @@ const Home = ({ setOverlay }: { setOverlay: any }) => {
             <Clock />
             <Date />
           </div>
-          <Memes setOverlay={setOverlay} />
+          {miniApps[miniApp]}
         </div>
         <div className="flex flex-col items-end">
           <FavoriteSites />
