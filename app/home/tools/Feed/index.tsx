e4550ee33b0e0828036d66fa6e4cbc3918ba9e1b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 12:10:35 +0100

    Refactor Feed component styling

--- app/home/tools/Feed/index.tsx
+++ app/home/tools/Feed/index.tsx
@@ -9,15 +9,11 @@ const Feed = () => {
   const [source, setSource] = useState<string>("rss");
 
   return (
-    <div className="p-2 overflow-hidden bg-black bg-opacity-30 backdrop-blur-3xl h-full max-w-full rounded-xl w-full">
-      <div className="flex items-center justify-between pt-2">
+    <div className="p-2 overflow-hidden bg-black bg-opacity-30 backdrop-blur-3xl h-full max-w-full rounded-xl w-full shadow-xl">
+      <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold text-center pl-4">Feed</h1>
         <Nav
-          tabs={[
-            "rss",
-            "reddit",
-            "hackernews", // , "twitter", "youtube", "github" - later
-          ]}
+          tabs={["rss", "reddit", "hackernews"]} // , "twitter", "youtube", "github" - later
           tab={source}
           setTab={setSource}
           className="p-0 py-0"
