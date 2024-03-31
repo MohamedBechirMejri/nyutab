08114bab61ca78c35115961401f8a958ec3a31a4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 10:57:22 +0100

    Update function to handle edge case scenarios

---
+++ types/prayers.ts
@@ -0,0 +1,13 @@
+export type PrayerTimes = {
+  parsedDate: string;
+  date: string;
+  fajr: string;
+  sunrise: string;
+  dhuhr: string;
+  asr: string;
+  maghrib: string;
+  isha: string;
+  today: string;
+  isToday: boolean;
+  isTomorrow: boolean;
+};
