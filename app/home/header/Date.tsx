08ec052dedd9c3dae4f6ce4674b48b669c962d66
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 06:49:40 +0100

    Refactor import statement in Date.tsx

--- app/home/header/Date.tsx
+++ app/home/header/Date.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getDate } from "../../lib/dateUtils";
+import { getDate } from "lib/dateUtils";
 
 const Date = () => {
   const [currentDate, setCurrentDate] = useState(getDate().split(", "));
@@ -12,7 +12,7 @@ const Date = () => {
   }, []);
 
   return (
-    <h1 className="flex items-start justify-center 2xl:text-2xl font-bold text-center bg-transparent rounded-lg font-[FiraCode] flex-col w-max">
+    <h1 className="flex items-start justify-center font-bold text-center bg-transparent rounded-lg font-[FiraCode] flex-col w-max opacity-90">
       {currentDate[0]}, {currentDate[1]}
     </h1>
   );
