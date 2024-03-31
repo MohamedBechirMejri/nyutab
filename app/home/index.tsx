93d170b22b818f526ff3ec9a3b496c8868421eed
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 11:19:20 +0100

    Add tools and games buttons to home page

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -5,6 +5,17 @@ import Marquee from "./marquee";
 import Feed from "./tools/Feed";
 import Memes from "./tools/Memes";
 
+const tools = ["memes", "feed"];
+const games = [
+  "2048",
+  "countries",
+  "minesweeper",
+  "reflex challenge",
+  "sudoku",
+  "wordle",
+  "wordsearch",
+];
+
 const miniApps = {
   memes: Memes,
   feed: Feed,
@@ -25,18 +36,35 @@ const Home = () => {
   return (
     <div className="w-full h-full grid grid-rows-[minmax(0,1fr),auto]">
       <div className="grid grid-cols-[15vw,minmax(0,1fr),15vw] gap-8 grid-rows-1">
-        <div className="">
-          {Object.keys(miniApps).map(app => (
-            <button
-              key={app}
-              onClick={() => setMiniApp(app)}
-              className={`w-full p-4 text-white text-center ${
-                miniApp === app ? "bg-blue-500" : "bg-black"
-              }`}
-            >
-              {app}
-            </button>
-          ))}
+        <div className="flex flex-col justify-between p-4">
+          <div>
+            {tools.map(app => (
+              <button
+                key={app}
+                onClick={() => setMiniApp(app)}
+                className={`w-full p-4 text-white text-center ${
+                  miniApp === app ? "bg-blue-500" : "bg-black"
+                }`}
+              >
+                {app}
+              </button>
+            ))}
+          </div>
+
+          <div className="flex flex-wrap gap-x-4 gap-1">
+            <span className="w-full">Games:</span>
+            {games.map(app => (
+              <button
+                key={app}
+                onClick={() => setMiniApp(app)}
+                className={`w-max px-4 p-2 rounded-2xl text-black text-center text-xs ${
+                  miniApp === app ? "bg-blue-500" : "bg-white"
+                }`}
+              >
+                {app}
+              </button>
+            ))}
+          </div>
         </div>
         <div className="h-full grid grid-rows-[auto,minmax(0,1fr)] place-items-center py-4 gap-8">
           <Header />
