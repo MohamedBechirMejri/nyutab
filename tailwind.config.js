1db9266ff0b11935ca475ac737e3fd1465d6105d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 06:04:40 +0100

    Update content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./{app,components}/**/*.{js,ts,jsx,tsx}"],
   theme: {},
   plugins: [],
 };
