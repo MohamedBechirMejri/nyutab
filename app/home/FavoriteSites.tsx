6123c6886cc17508957101cdf3b82d792123ec94
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-04-01 00:34:43 +0100

    Update site image styling in FavoriteSites component

--- app/home/FavoriteSites.tsx
+++ app/home/FavoriteSites.tsx
@@ -14,13 +14,13 @@ const FavoriteSites = () => {
           href={site}
           className="flex items-center justify-center h-full overflow-hidden sm:h-14"
           initial={{ scale: 1 }}
-          whileHover={{ scale: 1.1 }}
+          whileHover={{ scale: 1.5 }}
           whileTap={{ scale: 0.9 }}
         >
           <img
             src={`https://www.google.com/s2/favicons?domain=${site}&sz=128`}
             alt={site}
-            className="h-8 w-8 rounded-full object-contain shadow-xl border border-zinc-500"
+            className="h-8 w-8 rounded-full object-cover shadow-xl border border-zinc-500 bg-gray-500 bg-opacity-50 backdrop-blur-3xl"
           />
         </m.a>
       ))}
