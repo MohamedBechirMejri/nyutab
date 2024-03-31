a69c1d475e86fd6e7052447b1923e1041dac60ad
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 12:36:43 +0100

    Refactor Feed component to use dynamic tabs

--- app/home/tools/Feed/index.tsx
+++ app/home/tools/Feed/index.tsx
@@ -5,19 +5,32 @@ import HackerNews from "./HackerNews";
 import Reddit from "./Reddit";
 import RSS from "./RSS";
 
+const tabs = ["rss", "reddit", "hackernews"];
+
 const Feed = () => {
   const [source, setSource] = useState<string>("rss");
 
   return (
-    <div className="p-2 overflow-hidden bg-black bg-opacity-30 backdrop-blur-3xl h-full max-w-full rounded-xl w-full shadow-xl">
+    <div className="p-4 px-6 overflow-hidden bg-black bg-opacity-30 backdrop-blur-3xl h-full max-w-full rounded-xl w-full shadow-xl">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-center pl-4">Feed</h1>
-        <Nav
-          tabs={["rss", "reddit", "hackernews"]} // , "twitter", "youtube", "github" - later
-          tab={source}
-          setTab={setSource}
-          className="p-0 py-0"
-        />
+        <h1 className="text-2xl font-bold text-center">Feed</h1>
+
+        <nav>
+          <ul className="flex items-center gap-4">
+            {tabs.map((t, i) => (
+              <li key={i}>
+                <button
+                  className={`p-2 px-4 text-lg font-bold uppercase ${
+                    source === t ? "text-cyan-400" : "text-gray-300"
+                  }`}
+                  onClick={() => setSource(t)}
+                >
+                  {t}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
 
       {source === "rss" && <RSS />}
