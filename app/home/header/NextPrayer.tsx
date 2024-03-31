e17b6f77d13f7951b5173a3c8c9bf0bc60fa9f63
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-31 07:44:39 +0100

    Remove font-bold class from NextPrayer component

--- app/home/header/NextPrayer.tsx
+++ app/home/header/NextPrayer.tsx
@@ -1,6 +1,6 @@
 export default function NextPrayer() {
   return (
-    <div className="flex items-center justify-center w-full h-full p-2 text-teal-400 font-bold">
+    <div className="flex items-center justify-center w-full h-full p-2">
       Fajr in 00:55
     </div>
   );
