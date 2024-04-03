b14a2070a93d75b3c5189e56fb04888b580436e9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-04-03 05:50:55 +0100

    Remove unnecessary code in RSS component

--- app/home/tools/Feed/RSS/index.tsx
+++ app/home/tools/Feed/RSS/index.tsx
@@ -113,15 +113,3 @@ export default function RSS() {
     </div>
   );
 }
-
-/*
-
-    .replace(
-          "http:%2F%2Fnyaa.tracker.wf:7777%2Fannounce&tr=udp:%2F%2Ftracker.coppersurfer.tk:6969%2Fannounce&tr=udp:%2F%2Ftracker.opentrackr.org:1337%2Fannounce&tr=udp:%2F%2F9.rarbg.to:2710%2Fannounce&tr=udp:%2F%2F9.rarbg.me:2710%2Fannounce&tr=udp:%2F%2Ftracker.leechers-paradise.org:6969%2Fannounce&tr=udp:%2F%2Ftracker.internetwarriors.net:1337%2Fannounce&tr=udp:%2F%2Ftracker.cyberia.is:6969%2Fannounce&tr=udp:%2F%2Fexodus.desync.com:6969%2Fannounce&tr=udp:%2F%2Ftracker3.itzmx.com:6961%2Fannounce&tr=udp:%2F%2Ftracker.torrent.eu.org:451%2Fannounce&tr=udp:%2F%2Ftracker.tiny-vps.com:6969%2Fannounce&tr=udp:%2F%2Fretracker.lanta-net.ru:2710%2Fannounce&tr=http:%2F%2Fopen.acgnxtracker.com:80%2Fannounce&tr=wss:%2F%2Ftracker.openwebtorrent.com",
-          ""
-        )
-        .replace("magnet:?xt=urn:btih:", ""),
-      episode: entry.title,
-      ...anime,
-
-*/
