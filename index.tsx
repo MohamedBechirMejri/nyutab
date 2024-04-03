6de09ee1dd1280228c81e3a8aa45ebc343b2d4dc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-04-03 06:18:27 +0100

    Add version check and update local storage

--- index.tsx
+++ index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import { LazyMotion, domAnimation } from "framer-motion";
+import { getLocalData, setLocalData } from "lib/storageUtils";
+
+const version = getLocalData("version");
+
+console.log("version", version);
+
+if (!version || version.v !== "1.0.0") {
+  localStorage.clear();
+  setLocalData("version", { v: "1.0.0" });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
