5fc63f00ad1919110343eef660e383091728959e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-07-22 17:22:35 +0100

    feat: Add lazy loading for images in FitGirl feed section

--- app/home/tools/Feed/FitGirl/index.tsx
+++ app/home/tools/Feed/FitGirl/index.tsx
@@ -27,47 +27,43 @@ export default function FitGirl() {
 
   return (
     <div className="flex h-full">
-      <nav className="pb-28 pt-2 p-6 h-full shrink-0 overflow-scroll noscroll"></nav>
-      <div className="h-full w-full">
-        <div className="flex flex-col gap-2 h-max pb-[8rem]">
-          {posts.length ? (
-            <Virtuoso
-              style={{ height: 400 }}
-              data={posts}
-              itemContent={(_, [id, post]) => (
-                <div
-                  key={id}
-                  className="flex items-start gap-4 p-4 font-bold rounded-2xl relative overflow-hidden"
-                >
-                  <img
-                    src={post.image}
-                    alt={post.title + "background"}
-                    className="h-full rounded overflow-hidden shrink-0 absolute top-0 left-0 w-full blur-3xl opacity-25"
-                  />
-                  <img
-                    src={post.image}
-                    alt={post.title + "background"}
-                    className="h-32 w-32 rounded-full object-cover shadow-xl border border-zinc-500"
-                  />
-                  <div className="flex flex-col gap-2">
-                    <h1 className="text-2xl font-bold">{post.title}</h1>
-                    <p>{post.info["Release Date"]}</p>
-                    <p>{post.info["Genres"]}</p>
-                    <p>{post.info["Languages"]}</p>
-                    <p>{post.info["Size"]}</p>
-                    <p>{post.info["Repack Size"]}</p>
-                    <p>{post.info["Crack"]}</p>
-                    <p>{post.info["Links"]}</p>
-                  </div>
+      <div className="flex flex-col gap-2 h-full w-full">
+        {posts.length ? (
+          <Virtuoso
+            data={posts}
+            itemContent={(_, [id, post]) => (
+              <div
+                key={id}
+                className="flex items-start gap-4 p-4 font-bold rounded-2xl relative overflow-hidden"
+              >
+                <img
+                  src={post.image}
+                  alt={post.title + "background"}
+                  className="h-full rounded overflow-hidden shrink-0 absolute top-0 left-0 w-full blur-3xl opacity-25"
+                />
+                <img
+                  src={post.image}
+                  alt={post.title + "background"}
+                  className="h-32 w-32 rounded-full object-cover shadow-xl border border-zinc-500"
+                />
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-2xl font-bold">{post.title}</h1>
+                  <p>{post.info["Release Date"]}</p>
+                  <p>{post.info["Genres"]}</p>
+                  <p>{post.info["Languages"]}</p>
+                  <p>{post.info["Size"]}</p>
+                  <p>{post.info["Repack Size"]}</p>
+                  <p>{post.info["Crack"]}</p>
+                  <p>{post.info["Links"]}</p>
                 </div>
-              )}
-            />
-          ) : (
-            <div className="flex items-center justify-center h-full pt-[12rem] animate-pulse">
-              <span className="text-2xl font-bold text-center">Loading...</span>
-            </div>
-          )}
-        </div>
+              </div>
+            )}
+          />
+        ) : (
+          <div className="flex items-center justify-center h-full pt-[12rem] animate-pulse">
+            <span className="text-2xl font-bold text-center">Loading...</span>
+          </div>
+        )}
       </div>
     </div>
   );
