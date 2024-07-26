9a27114bb1fc0097944b3b636d23814826ffccb8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-07-26 18:37:55 +0100

    feat: Update settings overlay UI and functionality
    This commit updates the UI and functionality of the settings overlay. It improves the layout and styling of the overlay, making it more visually appealing. Additionally, it adds a "Save Settings" button that allows users to save their settings. The changes enhance the user experience and make it easier for users to customize their settings.

--- app/overlay/settings/index.tsx
+++ app/overlay/settings/index.tsx
@@ -11,56 +11,56 @@ import Favorites from "./favorites";
 const sections = {
   feed: Feed,
   memes: Memes,
-  favorites: Favorites
+  favorites: Favorites,
 };
 
 const Settings = () => {
   const [section, setSection] = useState<SettingsTab>("feed");
 
   const { settings } = useSettingsStore();
-  const {setOverlay} = useOverlayStore();
+  const { setOverlay } = useOverlayStore();
 
   const saveSettings = () => {
-    setLocalData("settings", settings)
-    setOverlay(null)
+    setLocalData("settings", settings);
+    setOverlay(null);
   };
 
   const Section = sections[section];
 
   return (
-    <div className="grid w-full h-full grid-cols-1 grid-rows-6 shadow-2xl select-none rounded-xl bg-gradient-to-tr from-zinc-700 to-zinc-900 z-50 fixed">
+    <div className="fixed left-0 top-0 h-full w-[40%] bg-black/70 backdrop-blur-3xl grid grid-rows-[auto,minmax(0,1fr),auto] p-4">
       <div className="flex items-center justify-end row-span-1 px-8">
-        <div className="grid grid-cols-[1fr,12rem] gap-8 pl-24">
-          <Nav tabs={Object.keys(sections)} tab={section} setTab={setSection} />
-          <m.button
-            initial={{
-              backgroundColor: "transparent",
-              color: "#000",
-              borderRadius: "2rem",
-              scale: 0.75,
-            }}
-            animate={{
-              backgroundColor: "#f9d423",
-              color: "#000",
-              scale: 1,
-            }}
-            whileHover={{
-              borderRadius: "1.5rem",
-              backgroundColor: "#e84899",
-              backgroundImage:
-                "linear-gradient(315deg, #e84899 0%, #f9d423 74%)",
-            }}
-            whileTap={{ borderRadius: "2rem" }}
-            transition={{ type: "spring", damping: 10, stiffness: 100 }}
-            className="p-4 px-6 m-auto text-lg font-bold uppercase h-max w-max"
-            onClick={saveSettings}
-          >
-            Save Settings
-          </m.button>
-        </div>
+        <Nav tabs={Object.keys(sections)} tab={section} setTab={setSection} />
       </div>
       <div className="relative flex items-center justify-center w-full row-span-5 p-4 overflow-scroll">
         <Section />
+      </div>{" "}
+      <m.button
+        initial={{
+          backgroundColor: "transparent",
+          color: "#000",
+          borderRadius: "2rem",
+          scale: 0.75,
+        }}
+        animate={{
+          backgroundColor: "#f9d423",
+          color: "#000",
+          scale: 1,
+        }}
+        whileHover={{
+          borderRadius: "1.5rem",
+          backgroundColor: "#e84899",
+          backgroundImage: "linear-gradient(315deg, #e84899 0%, #f9d423 74%)",
+        }}
+        whileTap={{ borderRadius: "2rem" }}
+        transition={{ type: "spring", damping: 10, stiffness: 100 }}
+        className="p-4 px-6 m-auto text-lg font-bold uppercase h-max w-max"
+        onClick={saveSettings}
+      >
+        Save Settings
+      </m.button>
+      <div className="text-white absolute top-1/2 left-1/2 -rotate-45 text-9xl -translate-x-1/2 -translate-y-1/2 font-extrabold opacity-20 pointer-events-none -z-10">
+        WIP
       </div>
     </div>
   );
