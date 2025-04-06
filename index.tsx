c16fd523110b1c1107e9474e1ce4b778d27d2d07
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-04-06 16:16:53 +0100

    feat(scan): Integrate react-scan for enhanced performance monitoring
    - Added react-scan dependency to package.json for performance optimization.
    - Implemented scan functionality in index.tsx, enabling performance monitoring based on environment variable.
    - Updated build script to ensure scan feature is disabled by default during production builds.
    - Updated bun.lockb to reflect the new dependency changes.

--- index.tsx
+++ index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import { LazyMotion, domAnimation } from "framer-motion";
+import { scan } from "react-scan";
+
+scan({
+  enabled: import.meta.env.VITE_SCAN_ENABLED === "true",
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
