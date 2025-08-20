f1993540113116ce2b13dda55287c7e23fc6af2c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2025-08-20 01:25:39 +0100

    style: Comment out CurrencyTracker and update Tool type to remove currency reference

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -5,7 +5,7 @@ import FavoriteSites from './FavoriteSites'
 import GameButton, { type Game } from './games/GameButton'
 import Header from './header'
 import Marquee from './marquee'
-import CurrencyTracker from './tools/CurrencyTracker'
+// import CurrencyTracker from './tools/CurrencyTracker'
 import Feed from './tools/Feed'
 import FitGirl from './tools/FitGirl'
 import Memes from './tools/Memes'
@@ -17,11 +17,17 @@ const miniApps = {
   feed: Feed,
   fitgirl: FitGirl,
   anime: SP,
-  currency: CurrencyTracker,
+  // currency: CurrencyTracker,
   settings: Feed, // a hack to make typescript happy
 } as Record<Tool, React.FC>
 
-const tools = ['memes', 'feed', 'fitgirl', 'anime', 'currency'] as Tool[]
+const tools = [
+  'memes',
+  'feed',
+  'fitgirl',
+  'anime',
+  //,'currency'
+] as Tool[]
 
 const games = [
   // "countries", FIXME: there's a bug and the ui is bad
@@ -44,7 +50,7 @@ const Home = () => {
     <div className="w-full h-full grid grid-rows-[minmax(0,1fr)_auto]">
       <div className="grid grid-cols-[15vw_minmax(0,1fr)_15vw] gap-8 grid-rows-1">
         <div className="flex flex-col justify-between p-4 select-none">
-          <div className="flex flex-wrap gap-x-4 gap-2">
+          <div className="flex flex-wrap gap-2 gap-x-4">
             <span className="w-full pb-2">Tools:</span>
             {tools.map(app => (
               <ToolButton key={app} app={app} onClick={() => setMiniApp(app)} miniApp={miniApp} />
@@ -57,7 +63,7 @@ const Home = () => {
             />
           </div>
 
-          <div className="flex flex-wrap gap-x-4 gap-1">
+          <div className="flex flex-wrap gap-1 gap-x-4">
             <span className="w-full pb-2">Games:</span>
             {games.map(app => (
               <GameButton
@@ -71,7 +77,7 @@ const Home = () => {
         </div>
         <div className="h-full grid grid-rows-[auto_minmax(0,1fr)] place-items-center py-4 gap-8">
           <Header />
-          <div className="h-full w-full rounded-xl overflow-hidden flex items-center justify-center p-8">
+          <div className="flex items-center justify-center w-full h-full p-8 overflow-hidden rounded-xl">
             <MiniApp />
           </div>
         </div>
